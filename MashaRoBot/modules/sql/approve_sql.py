45d2f597909f3be0b65a1d71cfe1f5a1b83bc494
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-09 00:06:40 +0530

    Update approve_sql.py

--- MashaRoBot/modules/sql/approve_sql.py
+++ MashaRoBot/modules/sql/approve_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Integer, func, distinct
 
-from SaitamaRobot.modules.sql import BASE, SESSION
+from MashaRoBot.modules.sql import BASE, SESSION
 
 
 class Approvals(BASE):
