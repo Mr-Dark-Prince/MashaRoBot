fd1c48316c3e5432ea32c20d60f94de7bf7b7161
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-09 02:07:03 +0530

    MashaRoBot🔥

--- MashaRoBot/modules/userinfo.py
+++ MashaRoBot/modules/userinfo.py
@@ -13,7 +13,7 @@ from telegram.ext.dispatcher import run_async
 from telegram.error import BadRequest
 from telegram.utils.helpers import escape_markdown, mention_html
 
-from SaitamaRobot import (
+from MashaRoBot import (
     DEV_USERS,
     OWNER_ID,
     DRAGONS,
@@ -24,15 +24,15 @@ from SaitamaRobot import (
     dispatcher,
     sw,
 )
-from SaitamaRobot.__main__ import STATS, TOKEN, USER_INFO
-import SaitamaRobot.modules.sql.userinfo_sql as sql
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.sql.global_bans_sql import is_user_gbanned
-from SaitamaRobot.modules.sql.afk_sql import is_afk, check_afk_status
-from SaitamaRobot.modules.sql.users_sql import get_user_num_chats
-from SaitamaRobot.modules.helper_funcs.chat_status import sudo_plus
-from SaitamaRobot.modules.helper_funcs.extraction import extract_user
-from SaitamaRobot import telethn as SaitamaTelethonClient, TIGERS, DRAGONS, DEMONS
+from MashaRoBot.__main__ import STATS, TOKEN, USER_INFO
+import MashaRoBot.modules.sql.userinfo_sql as sql
+from MashaRoBot.modules.disable import DisableAbleCommandHandler
+from MashaRoBot.modules.sql.global_bans_sql import is_user_gbanned
+from MashaRoBot.modules.sql.afk_sql import is_afk, check_afk_status
+from MashaRoBot.modules.sql.users_sql import get_user_num_chats
+from MashaRoBot.modules.helper_funcs.chat_status import sudo_plus
+from MashaRoBot.modules.helper_funcs.extraction import extract_user
+from MashaRoBot import telethn as SaitamaTelethonClient, TIGERS, DRAGONS, DEMONS
 
 
 def no_by_per(totalhp, percentage):
