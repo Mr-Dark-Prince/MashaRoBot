b1ccaf5faeb525dd07e63a6f6aa335bd779da5b5
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-09 01:24:37 +0530

    🙄ഇതെന്ത് 🦚

--- MashaRoBot/modules/eval.py
+++ MashaRoBot/modules/eval.py
@@ -6,8 +6,8 @@ import textwrap
 import traceback
 from contextlib import redirect_stdout
 
-from SaitamaRobot import LOGGER, dispatcher
-from SaitamaRobot.modules.helper_funcs.chat_status import dev_plus
+from MashaRoBot import LOGGER, dispatcher
+from MashaRoBot.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
