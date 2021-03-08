12af97a3fb2d454f51109812e746c8290e59fef7
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-08 23:52:54 +0530

    Update chat_status.py

--- MashaRoBot/modules/helper_funcs/chat_status.py
+++ MashaRoBot/modules/helper_funcs/chat_status.py
@@ -2,7 +2,7 @@ from time import perf_counter
 from functools import wraps
 from cachetools import TTLCache
 from threading import RLock
-from SaitamaRobot import (
+from MashaRoBot import (
     DEL_CMDS,
     DEV_USERS,
     DRAGONS,
