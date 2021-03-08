c675cbd4b365515174fd72dd78a3d2154beff87c
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-09 01:29:34 +0530

    Update get_common_chats.py

--- MashaRoBot/modules/get_common_chats.py
+++ MashaRoBot/modules/get_common_chats.py
@@ -1,9 +1,9 @@
 import os
 from time import sleep
 
-from SaitamaRobot import OWNER_ID, dispatcher
-from SaitamaRobot.modules.helper_funcs.extraction import extract_user
-from SaitamaRobot.modules.sql.users_sql import get_user_com_chats
+from MashaRoBot import OWNER_ID, dispatcher
+from MashaRoBot.modules.helper_funcs.extraction import extract_user
+from MashaRoBot.modules.sql.users_sql import get_user_com_chats
 from telegram import Update
 from telegram.error import BadRequest, RetryAfter, Unauthorized
 from telegram.ext import CallbackContext, CommandHandler, Filters
