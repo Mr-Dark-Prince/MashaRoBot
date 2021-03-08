e6a995331854f6ec4552a15e0c5a39fa41810d45
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-09 00:40:07 +0530

    MashaRoBot

--- MashaRoBot/modules/afk.py
+++ MashaRoBot/modules/afk.py
@@ -1,12 +1,12 @@
 import random, html
 
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import (
+from MashaRoBot import dispatcher
+from MashaRoBot.modules.disable import (
     DisableAbleCommandHandler,
     DisableAbleMessageHandler,
 )
-from SaitamaRobot.modules.sql import afk_sql as sql
-from SaitamaRobot.modules.users import get_user_id
+from MashaRoBot.modules.sql import afk_sql as sql
+from MashaRoBot.modules.users import get_user_id
 from telegram import MessageEntity, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, Filters, MessageHandler, run_async
