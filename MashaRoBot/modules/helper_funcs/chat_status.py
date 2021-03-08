b4110040e9db0bc819b40832c2311b16e32173d4
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-08 23:57:39 +0530

    😁ഒളിച്ചു ഇരിക്കുന്നോ 🙊

--- MashaRoBot/modules/helper_funcs/chat_status.py
+++ MashaRoBot/modules/helper_funcs/chat_status.py
@@ -403,6 +403,6 @@ def connection_status(func):
 
 
 # Workaround for circular import with connection.py
-from SaitamaRobot.modules import connection
+from MashaRoBot.modules import connection
 
 connected = connection.connected
