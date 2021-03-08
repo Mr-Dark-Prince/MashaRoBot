f31018a618ef576abb6ab39a0fa8e70115f9ef01
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-09 02:31:28 +0530

    Update disable.py

--- MashaRoBot/modules/disable.py
+++ MashaRoBot/modules/disable.py
@@ -235,7 +235,7 @@ if is_module_loaded(FILENAME):
         chat = update.effective_chat
 
         if len(args) >= 1:
-            enable_module = "SaitamaRobot.modules." + args[0].rsplit(".", 1)[0]
+            enable_module = "MashaRoBot.modules." + args[0].rsplit(".", 1)[0]
 
             try:
                 module = importlib.import_module(enable_module)
