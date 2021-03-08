ae5f8f4a9598c859f745dd73468c0423c3df92a3
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-09 00:54:04 +0530

    MashaRoBot ⚠️ Block users using this bot

--- MashaRoBot/modules/blacklistusers.py
+++ MashaRoBot/modules/blacklistusers.py
@@ -1,7 +1,7 @@
 # Module to blacklist users and prevent them from using commands by @TheRealPhoenix
 import html
-import SaitamaRobot.modules.sql.blacklistusers_sql as sql
-from SaitamaRobot import (
+import MashaRoBot.modules.sql.blacklistusers_sql as sql
+from MashaRoBot import (
     DEV_USERS,
     OWNER_ID,
     DRAGONS,
@@ -10,12 +10,12 @@ from SaitamaRobot import (
     WOLVES,
     dispatcher,
 )
-from SaitamaRobot.modules.helper_funcs.chat_status import dev_plus
-from SaitamaRobot.modules.helper_funcs.extraction import (
+from MashaRoBot.modules.helper_funcs.chat_status import dev_plus
+from MashaRoBot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from SaitamaRobot.modules.log_channel import gloggable
+from MashaRoBot.modules.log_channel import gloggable
 from telegram import ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
