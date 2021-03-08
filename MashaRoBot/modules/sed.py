be18814764d2c622cedad3fba8430f6984ed6560
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-09 02:01:09 +0530

    Update sed.py

--- MashaRoBot/modules/sed.py
+++ MashaRoBot/modules/sed.py
@@ -2,9 +2,9 @@ import sre_constants
 
 import regex
 import telegram
-from SaitamaRobot import LOGGER, dispatcher
-from SaitamaRobot.modules.disable import DisableAbleMessageHandler
-from SaitamaRobot.modules.helper_funcs.regex_helper import infinite_loop_check
+from MashaRoBot import LOGGER, dispatcher
+from MashaRoBot.modules.disable import DisableAbleMessageHandler
+from MashaRoBot.modules.helper_funcs.regex_helper import infinite_loop_check
 from telegram import Update
 from telegram.ext import CallbackContext, Filters, run_async
 
