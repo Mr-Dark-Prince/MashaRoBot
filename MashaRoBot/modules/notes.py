378f5895d5d8252aaddb0cc530acfb6f5338ae55
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-09 01:44:58 +0530

    MashaRoBot📚

--- MashaRoBot/modules/notes.py
+++ MashaRoBot/modules/notes.py
@@ -3,14 +3,14 @@ from io import BytesIO
 import random
 from typing import Optional
 
-import SaitamaRobot.modules.sql.notes_sql as sql
-from SaitamaRobot import LOGGER, JOIN_LOGGER, SUPPORT_CHAT, dispatcher, DRAGONS
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.handlers import MessageHandlerChecker
-from SaitamaRobot.modules.helper_funcs.chat_status import user_admin, connection_status
-from SaitamaRobot.modules.helper_funcs.misc import build_keyboard, revert_buttons
-from SaitamaRobot.modules.helper_funcs.msg_types import get_note_type
-from SaitamaRobot.modules.helper_funcs.string_handling import (
+import MashaRoBot.modules.sql.notes_sql as sql
+from MashaRoBot import LOGGER, JOIN_LOGGER, SUPPORT_CHAT, dispatcher, DRAGONS
+from MashaRoBot.modules.disable import DisableAbleCommandHandler
+from MashaRoBot.modules.helper_funcs.handlers import MessageHandlerChecker
+from MashaRoBot.modules.helper_funcs.chat_status import user_admin, connection_status
+from MashaRoBot.modules.helper_funcs.misc import build_keyboard, revert_buttons
+from MashaRoBot.modules.helper_funcs.msg_types import get_note_type
+from MashaRoBot.modules.helper_funcs.string_handling import (
     escape_invalid_curly_brackets,
 )
 from telegram import (
