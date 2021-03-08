5a76a8136131ddd580c00881af1cbccb29e963d5
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-08 23:33:25 +0530

    😹To MashaRoBot

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Saitama Robot
-:: Enables virtual env mode and then starts saitama
-env\scripts\activate.bat && py -m SaitamaRobot
+TITLE Masha Robot
+:: Enables virtual env mode and then starts Masha
+env\scripts\activate.bat && py -m MashaRoBot
