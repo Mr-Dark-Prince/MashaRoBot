d8a97c7b1b360bda0cbf64c245bf59f217706173
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-09 00:06:13 +0530

    Update antiflood_sql.py

--- MashaRoBot/modules/sql/antiflood_sql.py
+++ MashaRoBot/modules/sql/antiflood_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import String, Column, Integer, UnicodeText
 
-from SaitamaRobot.modules.sql import SESSION, BASE
+from MashaRoBot.modules.sql import SESSION, BASE
 
 DEF_COUNT = 1
 DEF_LIMIT = 0
