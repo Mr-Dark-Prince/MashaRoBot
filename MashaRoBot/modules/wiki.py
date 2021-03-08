2f4cb7b80005fe3aeb12108eb79af96d9e13a574
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-09 01:55:36 +0530

    MashaRoBot

--- MashaRoBot/modules/wiki.py
+++ MashaRoBot/modules/wiki.py
@@ -1,6 +1,6 @@
 import wikipedia
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from MashaRoBot import dispatcher
+from MashaRoBot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 from wikipedia.exceptions import DisambiguationError, PageError
