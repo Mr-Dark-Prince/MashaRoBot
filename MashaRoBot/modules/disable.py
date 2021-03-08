329a497383dffe5fd9bb0008b8f8b947bc3e0b38
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-09 01:21:11 +0530

    MashaRoBot🚶

--- MashaRoBot/modules/disable.py
+++ MashaRoBot/modules/disable.py
@@ -20,12 +20,12 @@ FILENAME = __name__.rsplit(".", 1)[-1]
 # If module is due to be loaded, then setup all the magical handlers
 if is_module_loaded(FILENAME):
 
-    from SaitamaRobot.modules.helper_funcs.chat_status import (
+    from MashaRoBot.modules.helper_funcs.chat_status import (
         connection_status,
         is_user_admin,
         user_admin,
     )
-    from SaitamaRobot.modules.sql import disable_sql as sql
+    from MashaRoBot.modules.sql import disable_sql as sql
     from telegram.ext.dispatcher import run_async
 
     DISABLE_CMDS = []
