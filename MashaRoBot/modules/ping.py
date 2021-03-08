537ce65c5820ac8065653b45fedfacf263de5f05
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-09 01:47:29 +0530

    Update ping.py

--- MashaRoBot/modules/ping.py
+++ MashaRoBot/modules/ping.py
@@ -5,9 +5,9 @@ import requests
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
-from SaitamaRobot import StartTime, dispatcher
-from SaitamaRobot.modules.helper_funcs.chat_status import sudo_plus
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from MashaRoBot import StartTime, dispatcher
+from MashaRoBot.modules.helper_funcs.chat_status import sudo_plus
+from MashaRoBot.modules.disable import DisableAbleCommandHandler
 
 sites_list = {
     "Telegram": "https://api.telegram.org",
