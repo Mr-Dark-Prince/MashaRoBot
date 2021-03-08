d58413287fdfa16f1e3d7958f51639e050f0db22
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-09 01:38:26 +0530

    MashaRoBot

--- MashaRoBot/modules/math.py
+++ MashaRoBot/modules/math.py
@@ -1,8 +1,8 @@
 import math
 
 import pynewtonmath as newton
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from MashaRoBot import dispatcher
+from MashaRoBot.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
