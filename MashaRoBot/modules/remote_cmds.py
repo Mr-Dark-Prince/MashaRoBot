a616b59985c737837d299efaa229ae12f9ec367f
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-09 01:51:22 +0530

    Update remote_cmds.py

--- MashaRoBot/modules/remote_cmds.py
+++ MashaRoBot/modules/remote_cmds.py
@@ -1,12 +1,12 @@
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.helper_funcs.chat_status import (
+from MashaRoBot import dispatcher
+from MashaRoBot.modules.helper_funcs.chat_status import (
     bot_admin,
     is_bot_admin,
     is_user_ban_protected,
     is_user_in_chat,
 )
-from SaitamaRobot.modules.helper_funcs.extraction import extract_user_and_text
-from SaitamaRobot.modules.helper_funcs.filters import CustomFilters
+from MashaRoBot.modules.helper_funcs.extraction import extract_user_and_text
+from MashaRoBot.modules.helper_funcs.filters import CustomFilters
 from telegram import Update, ChatPermissions
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
