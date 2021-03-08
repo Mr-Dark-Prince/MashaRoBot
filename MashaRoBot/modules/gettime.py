a9e04896b1261d4153094029343569e912a24660
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-09 01:31:16 +0530

    Update gettime.py

--- MashaRoBot/modules/gettime.py
+++ MashaRoBot/modules/gettime.py
@@ -2,8 +2,8 @@ import datetime
 from typing import List
 
 import requests
-from SaitamaRobot import TIME_API_KEY, dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from MashaRoBot import TIME_API_KEY, dispatcher
+from MashaRoBot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
