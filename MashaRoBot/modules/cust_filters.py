fae6a4ed95063adc1782f8aff6453ad2b1785770
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-09 01:12:15 +0530

    MashaRoBot⚠🍀

--- MashaRoBot/modules/cust_filters.py
+++ MashaRoBot/modules/cust_filters.py
@@ -15,25 +15,25 @@ from telegram.ext import (
 )
 from telegram.utils.helpers import mention_html, escape_markdown
 
-from SaitamaRobot import dispatcher, LOGGER, DRAGONS
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.handlers import MessageHandlerChecker
-from SaitamaRobot.modules.helper_funcs.chat_status import user_admin
-from SaitamaRobot.modules.helper_funcs.extraction import extract_text
-from SaitamaRobot.modules.helper_funcs.filters import CustomFilters
-from SaitamaRobot.modules.helper_funcs.misc import build_keyboard_parser
-from SaitamaRobot.modules.helper_funcs.msg_types import get_filter_type
-from SaitamaRobot.modules.helper_funcs.string_handling import (
+from MashaRoBot import dispatcher, LOGGER, DRAGONS
+from MashaRoBot.modules.disable import DisableAbleCommandHandler
+from MashaRoBot.modules.helper_funcs.handlers import MessageHandlerChecker
+from MashaRoBot.modules.helper_funcs.chat_status import user_admin
+from MashaRoBot.modules.helper_funcs.extraction import extract_text
+from MashaRoBot.modules.helper_funcs.filters import CustomFilters
+from MashaRoBot.modules.helper_funcs.misc import build_keyboard_parser
+from MashaRoBot.modules.helper_funcs.msg_types import get_filter_type
+from MashaRoBot.modules.helper_funcs.string_handling import (
     split_quotes,
     button_markdown_parser,
     escape_invalid_curly_brackets,
     markdown_to_html,
 )
-from SaitamaRobot.modules.sql import cust_filters_sql as sql
+from MashaRoBot.modules.sql import cust_filters_sql as sql
 
-from SaitamaRobot.modules.connection import connected
+from MashaRoBot.modules.connection import connected
 
-from SaitamaRobot.modules.helper_funcs.alternate import send_message, typing_action
+from MashaRoBot.modules.helper_funcs.alternate import send_message, typing_action
 
 HANDLER_GROUP = 10
 
