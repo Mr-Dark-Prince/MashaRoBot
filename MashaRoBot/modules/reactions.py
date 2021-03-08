3c822b0c3c84c2148f8c8873c08044f602acee81
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-09 01:49:58 +0530

    MashaRoBot😹

--- MashaRoBot/modules/reactions.py
+++ MashaRoBot/modules/reactions.py
@@ -1,7 +1,7 @@
 import random
 
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from MashaRoBot import dispatcher
+from MashaRoBot.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
