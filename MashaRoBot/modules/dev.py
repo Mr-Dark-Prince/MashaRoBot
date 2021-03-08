62d7a77d182086149e43e5938a41752a09847a91
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-09 02:28:33 +0530

    Update dev.py

--- MashaRoBot/modules/dev.py
+++ MashaRoBot/modules/dev.py
@@ -18,7 +18,7 @@ from telegram.ext import CallbackContext, CommandHandler, run_async
 def allow_groups(update: Update, context: CallbackContext):
     args = context.args
     if not args:
-        update.effective_message.reply_text(f"Current state: {SaitamaRobot.ALLOW_CHATS}")
+        update.effective_message.reply_text(f"Current state: {MashaRoBot.ALLOW_CHATS}")
         return
     if args[0].lower() in ["off", "no"]:
         MashaRoBot.ALLOW_CHATS = True
