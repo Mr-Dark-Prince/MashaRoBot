dbde9e548761c678ceec741561aae583aed7cea4
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-09 00:02:55 +0530

    Update chatstatus.py

--- MashaRoBot/modules/helper_funcs/telethn/chatstatus.py
+++ MashaRoBot/modules/helper_funcs/telethn/chatstatus.py
@@ -1,5 +1,5 @@
-from SaitamaRobot.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
-from SaitamaRobot import DRAGONS
+from MashaRoBot.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
+from MashaRoBot import DRAGONS
 from telethon.tl.types import ChannelParticipantsAdmins
 
 
