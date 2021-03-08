d9a50a5c6f223e19f6f5462cac740e07659182b2
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-09 02:07:54 +0530

    Update users.py

--- MashaRoBot/modules/users.py
+++ MashaRoBot/modules/users.py
@@ -11,10 +11,10 @@ from telegram.ext import (
     run_async,
 )
 
-import SaitamaRobot.modules.sql.users_sql as sql
-from SaitamaRobot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
-from SaitamaRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
-from SaitamaRobot.modules.sql.users_sql import get_all_users
+import MashaRoBot.modules.sql.users_sql as sql
+from MashaRoBot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
+from MashaRoBot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from MashaRoBot.modules.sql.users_sql import get_all_users
 
 USERS_GROUP = 4
 CHAT_GROUP = 5
