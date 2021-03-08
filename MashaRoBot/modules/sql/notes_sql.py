14bd10de27e8b2baf42fdb6e02d69f6b04c9a5a5
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-09 00:16:05 +0530

    Update notes_sql.py

--- MashaRoBot/modules/sql/notes_sql.py
+++ MashaRoBot/modules/sql/notes_sql.py
@@ -1,8 +1,8 @@
 # Note: chat_id's are stored as strings because the int is too large to be stored in a PSQL database.
 import threading
 
-from SaitamaRobot.modules.helper_funcs.msg_types import Types
-from SaitamaRobot.modules.sql import BASE, SESSION
+from MashaRoBot.modules.helper_funcs.msg_types import Types
+from MashaRoBot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText, distinct, func
 
 
