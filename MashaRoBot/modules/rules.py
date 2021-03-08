bf99c46e9ea989af6d3bdea6c909288e795299fc
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-09 01:57:00 +0530

    Update rules.py

--- MashaRoBot/modules/rules.py
+++ MashaRoBot/modules/rules.py
@@ -1,9 +1,9 @@
 from typing import Optional
 
-import SaitamaRobot.modules.sql.rules_sql as sql
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.helper_funcs.chat_status import user_admin
-from SaitamaRobot.modules.helper_funcs.string_handling import markdown_parser
+import MashaRoBot.modules.sql.rules_sql as sql
+from MashaRoBot import dispatcher
+from MashaRoBot.modules.helper_funcs.chat_status import user_admin
+from MashaRoBot.modules.helper_funcs.string_handling import markdown_parser
 from telegram import (
     InlineKeyboardButton,
     InlineKeyboardMarkup,
