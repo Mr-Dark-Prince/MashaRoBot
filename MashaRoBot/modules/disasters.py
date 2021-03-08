9a0596fe050ea35394d7647607344fc5d8231a23
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-09 01:22:21 +0530

    MashaRoBot

--- MashaRoBot/modules/disasters.py
+++ MashaRoBot/modules/disasters.py
@@ -3,7 +3,7 @@ import json
 import os
 from typing import Optional
 
-from SaitamaRobot import (
+from MashaRoBot import (
     DEV_USERS,
     OWNER_ID,
     DRAGONS,
@@ -13,13 +13,13 @@ from SaitamaRobot import (
     WOLVES,
     dispatcher,
 )
-from SaitamaRobot.modules.helper_funcs.chat_status import (
+from MashaRoBot.modules.helper_funcs.chat_status import (
     dev_plus,
     sudo_plus,
     whitelist_plus,
 )
-from SaitamaRobot.modules.helper_funcs.extraction import extract_user
-from SaitamaRobot.modules.log_channel import gloggable
+from MashaRoBot.modules.helper_funcs.extraction import extract_user
+from MashaRoBot.modules.log_channel import gloggable
 from telegram import ParseMode, TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 from telegram.utils.helpers import mention_html
