c7477ad5b594af6948a320e4a3d5a1ac8f91c4b9
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-08 23:53:51 +0530

    Update extraction.py

--- MashaRoBot/modules/helper_funcs/extraction.py
+++ MashaRoBot/modules/helper_funcs/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from SaitamaRobot import LOGGER
-from SaitamaRobot.modules.users import get_user_id
+from MashaRoBot import LOGGER
+from MashaRoBot.modules.users import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
