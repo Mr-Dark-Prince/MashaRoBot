9e4e2e1faa974f3eab0a8746d2d547cfd3d77902
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-09 00:16:47 +0530

    Update reporting_sql.py

--- MashaRoBot/modules/sql/reporting_sql.py
+++ MashaRoBot/modules/sql/reporting_sql.py
@@ -1,7 +1,7 @@
 import threading
 from typing import Union
 
-from SaitamaRobot.modules.sql import BASE, SESSION
+from MashaRoBot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String
 
 
