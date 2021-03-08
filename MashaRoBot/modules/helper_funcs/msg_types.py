6073ae5ad97d64264f6fc123d1daefdc9f2c4858
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-08 23:55:57 +0530

    Update msg_types.py

--- MashaRoBot/modules/helper_funcs/msg_types.py
+++ MashaRoBot/modules/helper_funcs/msg_types.py
@@ -1,6 +1,6 @@
 from enum import IntEnum, unique
 
-from SaitamaRobot.modules.helper_funcs.string_handling import button_markdown_parser
+from MashaRoBot.modules.helper_funcs.string_handling import button_markdown_parser
 from telegram import Message
 
 
