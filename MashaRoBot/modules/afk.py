bec7ab8e3b6bfba4d4c21740776469c893e7b65a
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-10 00:36:29 +0530

    🛒Removed afk help String and added to misc

--- MashaRoBot/modules/afk.py
+++ MashaRoBot/modules/afk.py
@@ -138,11 +138,6 @@ def check_afk(update, context, user_id, fst_name, userc_id):
             update.effective_message.reply_text(res, parse_mode="html")
 
 
-__help__ = """
- • `/afk <reason>`*:* mark yourself as AFK(away from keyboard).
- • `brb <reason>`*:* same as the afk command - but not a command.
-When marked as AFK, any mentions will be replied to with a message to say you're not available!
-"""
 
 AFK_HANDLER = DisableAbleCommandHandler("afk", afk)
 AFK_REGEX_HANDLER = DisableAbleMessageHandler(
