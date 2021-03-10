13f7b8a0d775163d164857dc3d9c7ad385234c69
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-11 02:21:04 +0530

    Update reporting.py

--- MashaRoBot/modules/reporting.py
+++ MashaRoBot/modules/reporting.py
@@ -285,7 +285,7 @@ dispatcher.add_handler(SETTING_HANDLER)
 dispatcher.add_handler(REPORT_HANDLER, REPORT_GROUP)
 dispatcher.add_handler(ADMIN_REPORT_HANDLER, REPORT_GROUP)
 
-__mod_name__ = "REPORTING"
+__mod_name__ = "REPORTS"
 __handlers__ = [
     (REPORT_HANDLER, REPORT_GROUP),
     (ADMIN_REPORT_HANDLER, REPORT_GROUP),
