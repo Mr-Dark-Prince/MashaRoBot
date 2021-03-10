e10a7e1c0793d7ab4d332b338302ee487d3f44ac
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-10 08:26:16 +0530

    Update backups.py

--- MashaRoBot/modules/backups.py
+++ MashaRoBot/modules/backups.py
@@ -367,7 +367,7 @@ def get_chat(chat_id, chat_data):
         return {"status": False, "value": False}
 
 
-__mod_name__ = "Backups"
+__mod_name__ = "BACKUPS"
 
 __help__ = """
 *Only for group owner:*
