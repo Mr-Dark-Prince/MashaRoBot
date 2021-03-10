5de580ea4d7ebf61259e594a5bb49825a70aeccc
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-10 14:14:34 +0530

    Update gtranslator.py

--- MashaRoBot/modules/gtranslator.py
+++ MashaRoBot/modules/gtranslator.py
@@ -95,12 +95,20 @@ __help__ = """
 *Example:* 
   `/tr en`*:* translates something to english
   `/tr hi-en`*:* translates hindi to english
+
+*Language Codes*
+`af,am,ar,az,be,bg,bn,bs,ca,ceb,co,cs,cy,da,de,el,en,eo,es,
+et,eu,fa,fi,fr,fy,ga,gd,gl,gu,ha,haw,hi,hmn,hr,ht,hu,hy,
+id,ig,is,it,iw,ja,jw,ka,kk,km,kn,ko,ku,ky,la,lb,lo,lt,lv,mg,mi,mk,
+ml,mn,mr,ms,mt,my,ne,nl,no,ny,pa,pl,ps,pt,ro,ru,sd,si,sk,sl,
+sm,sn,so,sq,sr,st,su,sv,sw,ta,te,tg,th,tl,tr,uk,ur,uz,
+vi,xh,yi,yo,zh,zh_CN,zh_TW,zu`
 """
 
 TRANSLATE_HANDLER = DisableAbleCommandHandler(["tr", "tl"], totranslate)
 
 dispatcher.add_handler(TRANSLATE_HANDLER)
 
-__mod_name__ = "TRANSLATOR"
+__mod_name__ = "TRST"
 __command_list__ = ["tr", "tl"]
 __handlers__ = [TRANSLATE_HANDLER]
