eb37abb3004a12b16da787f89f7e2c90cfb14454
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-10 13:56:39 +0530

    Capital letter

--- MashaRoBot/modules/cust_filters.py
+++ MashaRoBot/modules/cust_filters.py
@@ -639,7 +639,7 @@ Check `/markdownhelp` to know more!
 
 """
 
-__mod_name__ = "Filters"
+__mod_name__ = "FILTERS"
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
