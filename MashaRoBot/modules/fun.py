93a0ebd7677d24f6891efbeeb4c15004f7c0884a
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-10 14:06:15 +0530

    😾useless

--- MashaRoBot/modules/fun.py
+++ MashaRoBot/modules/fun.py
@@ -371,7 +371,7 @@ dispatcher.add_handler(DECIDE_HANDLER)
 dispatcher.add_handler(EIGHTBALL_HANDLER)
 dispatcher.add_handler(TABLE_HANDLER)
 
-__mod_name__ = "Fun"
+__mod_name__ = "MEMES"
 __command_list__ = [
     "runs",
     "slap",
