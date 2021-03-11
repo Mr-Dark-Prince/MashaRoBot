21d129deddd9939bc7b673d5a7766f827e5ed866
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-11 06:07:44 +0530

    Update gtranslator.py

--- MashaRoBot/modules/gtranslator.py
+++ MashaRoBot/modules/gtranslator.py
@@ -109,6 +109,6 @@ TRANSLATE_HANDLER = DisableAbleCommandHandler(["tr", "tl"], totranslate)
 
 dispatcher.add_handler(TRANSLATE_HANDLER)
 
-__mod_name__ = "G-TR"
+__mod_name__ = "G-TRANS"
 __command_list__ = ["tr", "tl"]
 __handlers__ = [TRANSLATE_HANDLER]
