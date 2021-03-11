7c7db163e7a31906bf36d2b70f321c2d6903a1fb
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-11 11:50:11 +0530

    Update chatbot.py

--- MashaRoBot/modules/chatbot.py
+++ MashaRoBot/modules/chatbot.py
@@ -146,8 +146,8 @@ Chatbot utilizes the CoffeeHouse API and allows Saitama to talk and provides a m
 
 *Commands:* 
 *Admins only:*
- • `/addchat`*:* Enables Chatbot mode in the chat.
- • `/rmchat`*:* Disables Chatbot mode in the chat.
+ ❍ /addchat*:* Enables Chatbot mode in the chat.
+ ❍ /rmchat*:* Disables Chatbot mode in the chat.
 
 Reports bugs at @{SUPPORT_CHAT}
 *Powered by CoffeeHouse* (https://coffeehouse.intellivoid.net/) from @Intellivoid
