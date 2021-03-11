a8c83f9d104184fa3e95a7490592a797526502d6
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-11 13:01:23 +0530

    Update disasters.py

--- MashaRoBot/modules/disasters.py
+++ MashaRoBot/modules/disasters.py
@@ -556,40 +556,40 @@ Commands listed here only work for users with special access are mainly used for
 Group admins/group owners do not need these commands. 
 
  ╔ *List all special users:*
- ╠ `/dragons`*:* Lists all Dragon disasters
- ╠ `/demons`*:* Lists all Demon disasters
- ╠ `/tigers`*:* Lists all Tigers disasters
- ╠ `/wolves`*:* Lists all Wolf disasters
- ╠ `/heroes`*:* Lists all Hero Association members
- ╠ `/adddragon`*:* Adds a user to Dragon
- ╠ `/adddemon`*:* Adds a user to Demon
- ╠ `/addtiger`*:* Adds a user to Tiger
- ╠ `/addwolf`*:* Adds a user to Wolf
+ ╠ /dragons*:* Lists all Dragon disasters
+ ╠ /demons*:* Lists all Demon disasters
+ ╠ /tigers*:* Lists all Tigers disasters
+ ╠ /wolves*:* Lists all Wolf disasters
+ ╠ /heroes*:* Lists all Hero Association members
+ ╠ /adddragon*:* Adds a user to Dragon
+ ╠ /adddemon*:* Adds a user to Demon
+ ╠ /addtiger*:* Adds a user to Tiger
+ ╠ /addwolf*:* Adds a user to Wolf
  ╚ `Add dev doesnt exist, devs should know how to add themselves`
 
  ╔ *Ping:*
- ╠ `/ping`*:* gets ping time of bot to telegram server
- ╚ `/pingall`*:* gets all listed ping times
+ ╠ /ping*:* gets ping time of bot to telegram server
+ ╚ /pingall*:* gets all listed ping times
 
  ╔ *Broadcast: (Bot owner only)*
  ╠  *Note:* This supports basic markdown
- ╠ `/broadcastall`*:* Broadcasts everywhere
- ╠ `/broadcastusers`*:* Broadcasts too all users
- ╚ `/broadcastgroups`*:* Broadcasts too all groups
+ ╠ /broadcastall*:* Broadcasts everywhere
+ ╠ /broadcastusers*:* Broadcasts too all users
+ ╚ /broadcastgroups*:* Broadcasts too all groups
 
  ╔ *Groups Info:*
- ╠ `/groups`*:* List the groups with Name, ID, members count as a txt
- ╠ `/leave <ID>`*:* Leave the group, ID must have hyphen
- ╠ `/stats`*:* Shows overall bot stats
- ╠ `/getchats`*:* Gets a list of group names the user has been seen in. Bot owner only
- ╚ `/ginfo username/link/ID`*:* Pulls info panel for entire group
+ ╠ /groups*:* List the groups with Name, ID, members count as a txt
+ ╠ /leave <ID>*:* Leave the group, ID must have hyphen
+ ╠ /stats*:* Shows overall bot stats
+ ╠ /getchats*:* Gets a list of group names the user has been seen in. Bot owner only
+ ╚ /ginfo username/link/ID*:* Pulls info panel for entire group
 
  ╔ *Access control:* 
- ╠ `/ignore`*:* Blacklists a user from 
+ ╠ /ignore*:* Blacklists a user from 
  ╠  using the bot entirely
- ╠ `/lockdown <off/on>`*:* Toggles bot adding to groups
- ╠ `/notice`*:* Removes user from blacklist
- ╚ `/ignoredlist`*:* Lists ignored users
+ ╠ /lockdown <off/on>*:* Toggles bot adding to groups
+ ╠ /notice*:* Removes user from blacklist
+ ╚ /ignoredlist*:* Lists ignored users
 
  ╔ *Module loading:*
  ╠ `/listmodules`*:* Prints modules and their names
