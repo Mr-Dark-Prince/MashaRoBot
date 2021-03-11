b3ad72fca91853c512b94a9471ea2a991ce85c44
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-11 14:21:37 +0530

    Update misc.py

--- MashaRoBot/modules/misc.py
+++ MashaRoBot/modules/misc.py
@@ -84,19 +84,19 @@ def markdown_help(update: Update, context: CallbackContext):
 __help__ = """
 *Available commands:*
 *Markdown:*
- • `/markdownhelp`*:* quick summary of how markdown works in telegram - can only be called in private chats
+ ❍ /markdownhelp*:* quick summary of how markdown works in telegram - can only be called in private chats
 *Paste:*
- • `/paste`*:* Saves replied content to `nekobin.com` and replies with a url
+ ❍ /paste*:* Saves replied content to `nekobin.com` and replies with a url
 *React:*
- • `/react`*:* Reacts with a random reaction 
+ ❍ /react*:* Reacts with a random reaction 
 *Urban Dictonary:*
- • `/ud <word>`*:* Type the word or expression you want to search use
+ ❍ /ud <word>*:* Type the word or expression you want to search use
 *Wikipedia:*
- • `/wiki <query>`*:* wikipedia your query
+ ❍ /wiki <query>*:* wikipedia your query
 *Wallpapers:*
- • `/wall <query>`*:* get a wallpaper from wall.alphacoders.com
+ ❍ /wall <query>*:* get a wallpaper from wall.alphacoders.com
 *Currency converter:* 
- • `/cash`*:* currency converter
+ ❍ /cash*:* currency converter
 Example:
  `/cash 1 USD INR`  
       _OR_
@@ -105,21 +105,21 @@ Output: `1.0 USD = 75.505 INR`
 
 *MATHS*
 Solves complex math problems using https://newton.now.sh
- • `/math`*:* Math `/math 2^2+2(2)`
- • `/factor`*:* Factor `/factor x^2 + 2x`
- • `/derive`*:* Derive `/derive x^2+2x`
- • `/integrate`*:* Integrate `/integrate x^2+2x`
- • `/zeroes`*:* Find 0's `/zeroes x^2+2x`
- • `/tangent`*:* Find Tangent `/tangent 2lx^3`
- • `/area`*:* Area Under Curve `/area 2:4lx^3`
- • `/cos`*:* Cosine `/cos pi`
- • `/sin`*:* Sine `/sin 0`
- • `/tan`*:* Tangent `/tan 0`
- • `/arccos`*:* Inverse Cosine `/arccos 1`
- • `/arcsin`*:* Inverse Sine `/arcsin 0`
- • `/arctan`*:* Inverse Tangent `/arctan 0`
- • `/abs`*:* Absolute Value `/abs -1`
- • `/log`*:* Logarithm `/log 2l8`
+ ❍ `/math`*:* Math `/math 2^2+2(2)`
+ ❍ `/factor`*:* Factor `/factor x^2 + 2x`
+ ❍ `/derive`*:* Derive `/derive x^2+2x`
+ ❍ `/integrate`*:* Integrate `/integrate x^2+2x`
+ ❍ `/zeroes`*:* Find 0's `/zeroes x^2+2x`
+ ❍ `/tangent`*:* Find Tangent `/tangent 2lx^3`
+ ❍ `/area`*:* Area Under Curve `/area 2:4lx^3`
+ ❍ `/cos`*:* Cosine `/cos pi`
+ ❍ `/sin`*:* Sine `/sin 0`
+ ❍ `/tan`*:* Tangent `/tan 0`
+ ❍ `/arccos`*:* Inverse Cosine `/arccos 1`
+ ❍ `/arcsin`*:* Inverse Sine `/arcsin 0`
+ ❍ `/arctan`*:* Inverse Tangent `/arctan 0`
+ ❍ `/abs`*:* Absolute Value `/abs -1`
+ ❍ `/log`*:* Logarithm `/log 2l8`
 
 _Keep in mind_: To find the tangent line of a function at a certain x value, send the request as c|f(x) where c is the given x value and f(x) is the function expression, the separator is a vertical bar '|'. See the table above for an example request.
 To find the area under a function, send the request as c:d|f(x) where c is the starting x value, d is the ending x value, and f(x) is the function under which you want the curve between the two x values.
