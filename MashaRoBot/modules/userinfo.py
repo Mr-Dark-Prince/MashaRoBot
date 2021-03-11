b36d58399444e4ff26887315c3fec914d3e60419
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-11 16:23:18 +0530

    Update userinfo.py

--- MashaRoBot/modules/userinfo.py
+++ MashaRoBot/modules/userinfo.py
@@ -518,30 +518,30 @@ def __user_info__(user_id):
 
 __help__ = """
 *Away from group*
- • `/afk <reason>`*:* mark yourself as AFK(away from keyboard).
- • `brb <reason>`*:* same as the afk command - but not a command.
+ ❍ /afk <reason>*:* mark yourself as AFK(away from keyboard).
+ ❍ brb <reason>*:* same as the afk command - but not a command.
 When marked as AFK, any mentions will be replied to with a message to say you're not available!
 
 *ID:*
- • `/id`*:* get the current group id. If used by replying to a message, gets that user's id.
- • `/gifid`*:* reply to a gif to me to tell you its file ID.
+ ❍ /id*:* get the current group id. If used by replying to a message, gets that user's id.
+ ❍ /gifid*:* reply to a gif to me to tell you its file ID.
 
 *Self addded information:* 
- • `/setme <text>`*:* will set your info
- • `/me`*:* will get your or another user's info.
+ ❍ /setme <text>*:* will set your info
+ ❍ /me*:* will get your or another user's info.
 Examples:
  `/setme I am a wolf.`
  `/me @username(defaults to yours if no user specified)`
 
 *Information others add on you:* 
- • `/bio`*:* will get your or another user's bio. This cannot be set by yourself.
-• `/setbio <text>`*:* while replying, will save another user's bio 
+ ❍ /bio`*:* will get your or another user's bio. This cannot be set by yourself.
+ ❍ /setbio <text>`*:* while replying, will save another user's bio 
 Examples:
  `/bio @username(defaults to yours if not specified).`
  `/setbio This user is a wolf` (reply to the user)
 
 *Overall Information about you:*
- • `/info`*:* get information about a user. 
+ ❍ /info*:* get information about a user. 
  
 *What is that health thingy?*
  Come and see [HP System explained](https://t.me/OnePunchUpdates/192)
