6946f604325480c7a07a73adedc32203cd59f8ba
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-12 16:42:04 +0530

    Update ___marsha_tools.py

--- MashaRoBot/modules/___marsha_tools.py
+++ MashaRoBot/modules/___marsha_tools.py
@@ -1,6 +1,6 @@
 # © @Mr_Dark_Prince
 
-__mod_name__ = "MASHAROBOT SPECIAL BOX"
+__mod_name__ = "▫️MASHAROBOT SPECIAL TOOLS▫️"
 
 __help__ = """
  ❍ /song [song name] :uploads the song in it's best quality available
