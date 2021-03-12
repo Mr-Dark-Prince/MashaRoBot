ffb1730655e8a97262ce9b70f598076310594d04
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-12 16:31:54 +0530

    ഇമോജി  വേണ്ട 🤧

--- MashaRoBot/modules/helper_funcs/misc.py
+++ MashaRoBot/modules/helper_funcs/misc.py
@@ -72,7 +72,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         pairs.append((modules[-1],))
 
     else:
-        pairs += [[EqInlineKeyboardButton("⬆️ GO BACK TO THE MAIN MENU ⬆️", callback_data="masha_back")]]
+        pairs += [[EqInlineKeyboardButton("⬆ GO BACK TO THE MAIN MENU ⬆", callback_data="masha_back")]]
 
     return pairs
 
