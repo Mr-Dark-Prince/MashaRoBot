6b7484e270d1bf2d6747125f9e6186fc9d73e88d
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-14 11:43:24 +0530

    Update __nightmode.py

--- MashaRoBot/modules/__nightmode.py
+++ MashaRoBot/modules/__nightmode.py
@@ -1,4 +1,4 @@
-from MashaRoBot.modules.sql.night_mode_sql import add_nightmode, rmnightmode, get_all_chat_id, is_nightmode_indb
+from MashaRoBot.modules.sql_extended.night_mode_sql import add_nightmode, rmnightmode, get_all_chat_id, is_nightmode_indb
 from telethon.tl.types import ChatBannedRights
 from apscheduler.schedulers.asyncio import AsyncIOScheduler 
 from telethon import functions
