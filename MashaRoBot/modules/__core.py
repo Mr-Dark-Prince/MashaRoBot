cf848fe43b0fb06d2932f1777e9a8e2403927f86
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-14 15:24:42 +0530

    Update __core.py

--- MashaRoBot/modules/__core.py
+++ MashaRoBot/modules/__core.py
@@ -34,3 +34,48 @@ async def Prof(event):
          )
     else:
         await event.reply("No File Found!")
+
+
+from MashaRoBot.events import load_module
+import asyncio
+import os
+from datetime import datetime
+from pathlib import Path
+
+@register(pattern="^/install")
+async def install(event):
+    if event.fwd_from:
+        return
+    if event.sender_id == OWNER_ID:
+        pass
+    else:
+        return
+    if event.reply_to_msg_id:
+        try:
+            downloaded_file_name = (
+                await event.client.download_media(  # pylint:disable=E0602
+                    await event.get_reply_message(),
+                    "MashaRoBot/modules/",  # pylint:disable=E0602
+                )
+            )
+            if "(" not in downloaded_file_name:
+                path1 = Path(downloaded_file_name)
+                shortname = path1.stem
+                load_module(shortname.replace(".py", ""))
+                await event.reply("Installed.... 👍\n `{}`".format(
+                        os.path.basename(downloaded_file_name)
+                    ),
+                )
+            else:
+                os.remove(downloaded_file_name)
+                k = await event.reply("**Error!**\n⚠️Cannot Install! \n📂 File not supported \n Or Pre Installed Maybe..😁",
+                )
+                await asyncio.sleep(2)
+                await k.delete()
+        except Exception as e:  # pylint:disable=C0103,W0703
+            j = await event.reply(str(e))
+            await asyncio.sleep(3)
+            await j.delete()
+            os.remove(downloaded_file_name)
+    await asyncio.sleep(3)
+    await event.delete()
