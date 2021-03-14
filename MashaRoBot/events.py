2ff6479ca5ae2cd19fa0475eadfac066be9972c9
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-14 15:50:20 +0530

    Update events.py

--- MashaRoBot/events.py
+++ MashaRoBot/events.py
@@ -67,6 +67,37 @@ def callbackquery(**args):
     return decorator
 
 
+def masharobot(**args):
+    pattern = args.get("pattern", None)
+    disable_edited = args.get("disable_edited", False)
+    ignore_unsafe = args.get("ignore_unsafe", False)
+    unsafe_pattern = r"^[^/!#@\$A-Za-z]"
+    group_only = args.get("group_only", False)
+    disable_errors = args.get("disable_errors", False)
+    insecure = args.get("insecure", False)
+    if pattern is not None and not pattern.startswith("(?i)"):
+        args["pattern"] = "(?i)" + pattern
+
+    if "disable_edited" in args:
+        del args["disable_edited"]
+
+    if "ignore_unsafe" in args:
+        del args["ignore_unsafe"]
+
+    if "group_only" in args:
+        del args["group_only"]
+
+    if "disable_errors" in args:
+        del args["disable_errors"]
+
+    if "insecure" in args:
+        del args["insecure"]
+
+    if pattern:
+        if not ignore_unsafe:
+            args["pattern"] = args["pattern"].replace("^.", unsafe_pattern, 1)
+
+
 def load_module(shortname):
     if shortname.startswith("__"):
         pass
