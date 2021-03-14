e7f01f86afa7125131ee308d923081b645394b31
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-14 15:32:34 +0530

    Update events.py

--- MashaRoBot/events.py
+++ MashaRoBot/events.py
@@ -63,3 +63,42 @@ def callbackquery(**args):
         return func
 
     return decorator
+
+
+def load_module(shortname):
+    if shortname.startswith("__"):
+        pass
+    elif shortname.endswith("_"):
+        import importlib
+        import MashaRoBot.events
+
+        path = Path(f"MashaRoBot/modules/{shortname}.py")
+        name = "MashaRoBot.modules.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+        print("Successfully imported " + shortname)
+    else:
+        import importlib
+        import MashaRoBot.events
+
+        path = Path(f"MashaRoBot/modules/{shortname}.py")
+        name = "MashaRoBot.modules.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        mod.register = register
+        mod.masharobot = masharobot
+        mod.tbot = telethn
+        mod.logger = logging.getLogger(shortname)
+        spec.loader.exec_module(mod)
+        sys.modules["MashaRoBot.modules." + shortname] = mod
+        print("Successfully imported " + shortname)
+
+
+path = "MashaRoBot/modules/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as f:
+        path1 = Path(f.name)
+        shortname = path1.stem
+        load_module(shortname.replace(".py", ""))
