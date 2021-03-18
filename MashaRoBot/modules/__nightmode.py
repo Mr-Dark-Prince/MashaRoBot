a9fbf67d803f7f647f3655a0ea7ba69eb7703fa7
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-18 21:30:40 +0530

    Update __nightmode.py

--- MashaRoBot/modules/__nightmode.py
+++ MashaRoBot/modules/__nightmode.py
@@ -12,13 +12,13 @@ async def is_register_admin(chat, user):
     if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
 
         return isinstance(
-            (await telethn(functions.channels.GetParticipantRequest(chat, user))).participant,
+            (await tbot(functions.channels.GetParticipantRequest(chat, user))).participant,
             (types.ChannelParticipantAdmin, types.ChannelParticipantCreator)
         )
     elif isinstance(chat, types.InputPeerChat):
 
-        ui = await telethn.get_peer_id(user)
-        ps = (await telethn(functions.messages.GetFullChatRequest(chat.chat_id))) \
+        ui = await tbot.get_peer_id(user)
+        ps = (await tbot(functions.messages.GetFullChatRequest(chat.chat_id))) \
             .full_chat.participants.participants
         return isinstance(
             next((p for p in ps if p.user_id == ui), None),
