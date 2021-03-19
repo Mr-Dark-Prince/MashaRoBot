a6a6e146eacde280cd0ae82b7e51492d55a7920e
Mr-Dark-Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-19 05:22:15 +0000

    👣MashaRoBot

--- MashaRoBot/modules/feds.py
+++ MashaRoBot/modules/feds.py
@@ -1409,10 +1409,10 @@ def fed_ban_list(update: Update, context: CallbackContext):
                 backups += json.dumps(json_parser)
                 backups += "\n"
             with BytesIO(str.encode(backups)) as output:
-                output.name = "saitama_fbanned_users.json"
+                output.name = "masha_fbanned_users.json"
                 update.effective_message.reply_document(
                     document=output,
-                    filename="saitama_fbanned_users.json",
+                    filename="masha_fbanned_users.json",
                     caption="Total {} User are blocked by the Federation {}.".format(
                         len(getfban), info["fname"]
                     ),
@@ -1454,10 +1454,10 @@ def fed_ban_list(update: Update, context: CallbackContext):
                 )
                 backups += "\n"
             with BytesIO(str.encode(backups)) as output:
-                output.name = "saitama_fbanned_users.csv"
+                output.name = "masha_fbanned_users.csv"
                 update.effective_message.reply_document(
                     document=output,
-                    filename="saitama_fbanned_users.csv",
+                    filename="masha_fbanned_users.csv",
                     caption="Total {} User are blocked by Federation {}.".format(
                         len(getfban), info["fname"]
                     ),
--- MashaRoBot/modules/helper_funcs/telethn/chatstatus.py
+++ MashaRoBot/modules/helper_funcs/telethn/chatstatus.py
@@ -42,13 +42,13 @@ async def is_user_admin(user_id: int, chat_id):
     return status
 
 
-async def saitama_is_admin(chat_id: int):
+async def masha_is_admin(chat_id: int):
     status = False
-    saitama = await telethn.get_me()
+    masha = await telethn.get_me()
     async for user in telethn.iter_participants(
         chat_id, filter=ChannelParticipantsAdmins
     ):
-        if saitama.id == user.id:
+        if masha.id == user.id:
             status = True
             break
     return status
