288c90e92fecc888a1803b2b436170f4c3adfc57
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-21 01:46:44 +0530

    Update __telegraph.py

--- MashaRoBot/modules/__telegraph.py
+++ MashaRoBot/modules/__telegraph.py
@@ -1,48 +1,50 @@
-from MashaRoBot import telethn as tbot
 from MashaRoBot.events import register
+from MashaRoBot import telethn as tbot
+TMP_DOWNLOAD_DIRECTORY = "./"
+from telethon import events
 import os
-from datetime import datetime
-
-dir = './'
 from PIL import Image
-from telegraph import Telegraph, exceptions, upload_file
-
+from datetime import datetime
+from telegraph import Telegraph, upload_file, exceptions
+darkprince = "MASHA"
 telegraph = Telegraph()
-r = telegraph.create_account('Masha')
+r = telegraph.create_account(short_name=darkprince)
 auth_url = r["auth_url"]
 
 
 @register(pattern="^/t(m|t) ?(.*)")
-async def tele(event):
-   if event.fwd_from:
+async def _(event):
+    if event.fwd_from:
         return
-   optional_title = event.pattern_match.group(2)
-   if event.reply_to_msg_id:
+    optional_title = event.pattern_match.group(2)
+    if event.reply_to_msg_id:
         start = datetime.now()
         r_message = await event.get_reply_message()
         input_str = event.pattern_match.group(1)
         if input_str == "m":
             downloaded_file_name = await tbot.download_media(
-                r_message, dir
+                r_message,
+                TMP_DOWNLOAD_DIRECTORY
             )
+            end = datetime.now()
+            ms = (end - start).seconds
+            h = await event.reply("Downloaded to {} in {} seconds.".format(downloaded_file_name, ms))
             if downloaded_file_name.endswith((".webp")):
                 resize_image(downloaded_file_name)
             try:
+                start = datetime.now()
                 media_urls = upload_file(downloaded_file_name)
             except exceptions.TelegraphException as exc:
-                await event.reply("ERROR: " + str(exc))
+                await h.edit("ERROR: " + str(exc))
                 os.remove(downloaded_file_name)
             else:
+                end = datetime.now()
+                ms_two = (end - start).seconds
                 os.remove(downloaded_file_name)
-                await event.reply(
-                    "Uploaded to [Telegraph](https://telegra.ph{})".format(
-                        media_urls[0]
-                    ),
-                    link_preview=True,
-                )
+                await h.edit("Uploaded to https://telegra.ph{} in {} seconds.".format(media_urls[0], (ms + ms_two)), link_preview=True)
         elif input_str == "t":
             user_object = await tbot.get_entity(r_message.sender_id)
-            title_of_page = user_object.first_name  # + " " + user_object.last_name
+            title_of_page = user_object.first_name # + " " + user_object.last_name
             # apparently, all Users do not have last_name field
             if optional_title:
                 title_of_page = optional_title
@@ -51,7 +53,8 @@ async def tele(event):
                 if page_content != "":
                     title_of_page = page_content
                 downloaded_file_name = await tbot.download_media(
-                    r_message, dir
+                    r_message,
+                    TMP_DOWNLOAD_DIRECTORY
                 )
                 m_list = None
                 with open(downloaded_file_name, "rb") as fd:
@@ -60,17 +63,16 @@ async def tele(event):
                     page_content += m.decode("UTF-8") + "\n"
                 os.remove(downloaded_file_name)
             page_content = page_content.replace("\n", "<br>")
-            response = telegraph.create_page(title_of_page, html_content=page_content)
-            await event.edit(
-                "Pasted to [Telegraph](https://telegra.ph/{})".format(
-                    response["path"]
-                ),
-                link_preview=True,
+            response = telegraph.create_page(
+                title_of_page,
+                html_content=page_content
             )
-   else:
-        await event.reply(
-            "Reply to a message to get a permanent telegra.ph link."
-        )
+            end = datetime.now()
+            ms = (end - start).seconds
+            await event.reply("Pasted to https://telegra.ph/{} in {} seconds.".format(response["path"], ms), link_preview=True)
+    else:
+        await event.reply("Reply to a message to get a permanent telegra.ph link.")
+
 
 def resize_image(image):
     im = Image.open(image)
