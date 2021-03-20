42a1f6e27ee1ffde5fc58166fa9d5c5f8371bd71
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-21 02:29:19 +0530

    Update __tools.py

--- MashaRoBot/modules/__tools.py
+++ MashaRoBot/modules/__tools.py
@@ -1,15 +1,10 @@
 # © @Mr_Dark_Prince
 
-__mod_name__ = "◾️TOOLS"
+__mod_name__ = "▪TOOLS"
 
 __help__ = """
-❍ /github [username] :Get information about a GitHub user.
-❍ /country [country name] :Gathering info about given country
-❍ /imdb [Movie name] :Get full info about a movie with imdb.com
-❍ Masha [query] :Masha answers the query
-  💡Ex: `Masha where is India?`
-❍ /encrypt :Encrypts The Given Text
-❍ /decrypt :Decrypts Previously Ecrypted Text
-❍ /tm :Get Telegraph Link Of Replied Media
-❍ /tt :Get Telegraph Link of Replied Text
+ ❍ /encrypt :Encrypts The Given Text
+ ❍ /decrypt :Decrypts Previously Ecrypted Text
+ ❍ /tm :Get Telegraph Link Of Replied Media
+ ❍ /tt :Get Telegraph Link of Replied Text
 """
