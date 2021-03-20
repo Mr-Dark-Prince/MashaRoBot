cc384b4c3c2b81c2f2f4b35416d4a2c8157d9aa9
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-20 13:30:24 +0530

    🎬Imdb query added

--- MashaRoBot/modules/___masha_tools.py
+++ MashaRoBot/modules/___masha_tools.py
@@ -3,14 +3,16 @@
 __mod_name__ = "▫️MASHAROBOT SPECIAL TOOLS▫️"
 
 __help__ = """
- ❍ /song [song name] :uploads the song in it's best quality available
+ ❍ /song [song name]: Uploads the song in it's best quality available
  💡Ex: `/song Faded Alan Walker`
 
- ❍ /github [username] :Get information about a GitHub user.
+ ❍ /github [username]: Get information about a GitHub user.
 
- ❍ /country [country name] :Gathering info about given country
+ ❍ /country [country name]: Gathering info about given country
 
- ❍ Masha [query] :Masha answers the query
+ ❍ /imdb [Movie name]: Get full info about a movie with imdb.com
+
+ ❍ Masha [query]: Masha answers the query
   💡Ex: `Masha where is India?`
 
  ❍ /encrypt: Encrypts The Given Text
