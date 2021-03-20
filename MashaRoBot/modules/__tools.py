cc333b7297fc3848932582f801c9c2624e568ec8
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-20 22:14:08 +0530

    Update and rename ___masha_tools.py to __tools.py

--- MashaRoBot/modules/__tools.py
+++ MashaRoBot/modules/__tools.py
@@ -1,20 +1,13 @@
 # © @Mr_Dark_Prince
 
-__mod_name__ = "▫️MASHAROBOT SPECIAL TOOLS▫️"
+__mod_name__ = "◾️TOOLS"
 
 __help__ = """
- ❍ /song [song name] :Uploads the song in it's best quality available
- 💡Ex: `/song Faded Alan Walker`
-
  ❍ /github [username] :Get information about a GitHub user.
-
  ❍ /country [country name] :Gathering info about given country
-
  ❍ /imdb [Movie name] :Get full info about a movie with imdb.com
-
  ❍ Masha [query] :Masha answers the query
   💡Ex: `Masha where is India?`
-
  ❍ /encrypt :Encrypts The Given Text
  ❍ /decrypt :Decrypts Previously Ecrypted Text
 """
