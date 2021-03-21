b302606d2c8612a12a39dbc392c0143a6d4501af
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-21 15:12:24 +0530

    Update disasters.py

--- MashaRoBot/modules/disasters.py
+++ MashaRoBot/modules/disasters.py
@@ -584,51 +584,48 @@ Group admins/group owners do not need these commands.
  ❍ /getchats*:* Gets a list of group names the user has been seen in. Bot owner only
  ❍ /ginfo username/link/ID*:* Pulls info panel for entire group
 
- ╔ *Access control:* 
- ╠ /ignore*:* Blacklists a user from 
- ╠  using the bot entirely
- ╠ /lockdown <off/on>*:* Toggles bot adding to groups
- ╠ /notice*:* Removes user from blacklist
- ╚ /ignoredlist*:* Lists ignored users
+*Access control:* 
+ ❍ /ignore*:* Blacklists a user from using the bot entirely
+ ❍ /lockdown <off/on>*:* Toggles bot adding to groups
+ ❍ /notice*:* Removes user from blacklist
+ ❍ /ignoredlist*:* Lists ignored users
 
- ╔ *Speedtest:*
- ╚ /speedtest*:* Runs a speedtest and gives you 2 options to choose from, text or image output
+*Speedtest:*
+ ❍ /speedtest*:* Runs a speedtest and gives you 2 options to choose from, text or image output
 
- ╔ *Module loading:*
- ╠ /listmodules*:* Lists names of all modules
- ╠ /load modulename*:* Loads the said module to 
- ╠   memory without restarting.
- ╠ /unload modulename*:* Loads the said module from
- ╚   memory without restarting.memory without restarting the bot 
+*Module loading:*
+ ❍ /listmodules*:* Lists names of all modules
+ ❍ /load modulename*:* Loads the said module to memory without restarting.
+ ❍ /unload modulename*:* Loads the said module frommemory without restarting memory without restarting the bot 
 
- ╔ *Remote commands:*
- ╠ /rban user group*:* Remote ban
- ╠ /runban user group*:* Remote un-ban
- ╠ /rpunch user group*:* Remote punch
- ╠ /rmute user group*:* Remote mute
- ╚ /runmute user group*:* Remote un-mute
+*Remote commands:*
+ ❍ /rban user group*:* Remote ban
+ ❍ /runban user group*:* Remote un-ban
+ ❍ /rpunch user group*:* Remote punch
+ ❍ /rmute user group*:* Remote mute
+ ❍ /runmute user group*:* Remote un-mute
 
- ╔ *Windows self hosted only:*
- ╠ /reboot*:* Restarts the bots service
- ╚ /gitpull*:* Pulls the repo and then restarts the bots service
+*Windows self hosted only:*
+ ❍ /reboot*:* Restarts the bots service
+ ❍ /gitpull*:* Pulls the repo and then restarts the bots service
 
- ╔ *Chatbot:* 
- ╚ /listaichats*:* Lists the chats the chatmode is enabled in
+*Chatbot:* 
+ ❍ /listaichats*:* Lists the chats the chatmode is enabled in
  
- ╔ *Debugging and Shell:* 
- ╠ /debug <on/off>*:* Logs commands to updates.txt
- ╠ /logs*:* Run this in support group to get logs in pm
- ╠ /eval*:* Self explanatory
- ╠ /sh*:* Runs shell command
- ╠ /shell*:* Runs shell command
- ╠ /clearlocals*:* As the name goes
- ╠ /dbcleanup*:* Removes deleted accs and groups from db
- ╚ /py*:* Runs python code
+*Debugging and Shell:* 
+ ❍ /debug <on/off>*:* Logs commands to updates.txt
+ ❍ /logs*:* Run this in support group to get logs in pm
+ ❍ /eval*:* Self explanatory
+ ❍ /sh*:* Runs shell command
+ ❍ /shell*:* Runs shell command
+ ❍ /clearlocals*:* As the name goes
+ ❍ /dbcleanup*:* Removes deleted accs and groups from db
+ ❍ /py*:* Runs python code
  
- ╔ *Global Bans:*
- ╠ /gban <id> <reason>*:* Gbans the user, works by reply too
- ╠ /ungban*:* Ungbans the user, same usage as gban
- ╚ /gbanlist*:* Outputs a list of gbanned users
+*Global Bans:*
+ ❍ /gban <id> <reason>*:* Gbans the user, works by reply too
+ ❍ /ungban*:* Ungbans the user, same usage as gban
+ ❍ /gbanlist*:* Outputs a list of gbanned users
 
  ╔ *Global Blue Text*
  ╠ /gignoreblue <word>*:* globally ignorea bluetext cleaning of saved word across MashaRoBot.
