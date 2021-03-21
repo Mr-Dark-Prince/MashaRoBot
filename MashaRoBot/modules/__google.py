28b8d28ac29fff2cc1676ba78cc24c3765bf1aab
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-21 19:36:06 +0530

    Update __google.py

--- MashaRoBot/modules/__google.py
+++ MashaRoBot/modules/__google.py
@@ -275,18 +275,17 @@ async def apk(e):
         await e.reply("Exception Occured:- " + str(err))
 
 
-__mod_name__ = "▪SEARCH"
+__mod_name__ = "◎SEARCH"
 
 __help__ = """
- ❍ /google <text>: Perform a google search
- ❍ /img <text>: Search Google for images and returns them\nFor greater no. of results specify lim, For eg: `/img hello lim=10`
- ❍ /app <appname>: Searches for an app in Play Store and returns its details.
+ ❍ /google <text>*:* Perform a google search
+ ❍ /img <text>*:* Search Google for images and returns them\nFor greater no. of results specify lim, For eg: `/img hello lim=10`
+ ❍ /app <appname>*:* Searches for an app in Play Store and returns its details.
  ❍ /reverse: Does a reverse image search of the media which it was replied to.
- ❍ /github [username] :Get information about a GitHub user.
- ❍ /country [country name] :Gathering info about given country
- ❍ /imdb [Movie name] :Get full info about a movie with imdb.com
- ❍ Masha [query] :Masha answers the query
+ ❍ /gps <location>*:* Get gps location.
+ ❍ /github <username>*:* Get information about a GitHub user.
+ ❍ /country <country name>*:* Gathering info about given country
+ ❍ /imdb <Movie name>*:* Get full info about a movie with imdb.com
+ ❍ Masha <query>*:* Masha answers the query
   💡Ex: `Masha where is India?`
 """
-
-
