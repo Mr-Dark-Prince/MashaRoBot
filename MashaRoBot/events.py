d143ff273877138abe0b98c16128f39cd8dfe7e4
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-23 01:48:49 +0530

    Update events.py

--- MashaRoBot/events.py
+++ MashaRoBot/events.py
@@ -70,6 +70,74 @@ def callbackquery(**args):
     return decorator
 
 
+def bot(**args):
+    pattern = args.get("pattern")
+    r_pattern = r"^[/]"
+
+    if pattern is not None and not pattern.startswith("(?i)"):
+        args["pattern"] = "(?i)" + pattern
+
+    args["pattern"] = pattern.replace("^/", r_pattern, 1)
+    stack = inspect.stack()
+    previous_stack_frame = stack[1]
+    file_test = Path(previous_stack_frame.filename)
+    file_test = file_test.stem.replace(".py", "")
+    reg = re.compile("(.*)")
+
+    if pattern is not None:
+        try:
+            cmd = re.search(reg, pattern)
+            try:
+                cmd = cmd.group(1).replace("$", "").replace("\\", "").replace("^", "")
+            except BaseException:
+                pass
+
+            try:
+                FUN_LIST[file_test].append(cmd)
+            except BaseException:
+                FUN_LIST.update({file_test: [cmd]})
+        except BaseException:
+            pass
+
+    def decorator(func):
+        async def wrapper(check):
+            if check.edit_date:
+                return
+            if check.fwd_from:
+                return
+            if check.is_group or check.is_private:
+                pass
+            else:
+                print("i don't work in channels")
+                return
+            if check.is_group:
+               if check.chat.megagroup:
+                  pass
+               else:
+                  print("i don't work in small chats")
+                  return
+                          
+            users = gbanned.find({})
+            for c in users:
+                if check.sender_id == c["user"]:
+                    return
+            try:
+                await func(check)
+                try:
+                    LOAD_PLUG[file_test].append(func)
+                except Exception:
+                    LOAD_PLUG.update({file_test: [func]})
+            except BaseException:
+                return
+            else:
+                pass
+
+        telethn.add_event_handler(wrapper, events.NewMessage(**args))
+        return wrapper
+
+    return decorator
+
+
 def masharobot(**args):
     pattern = args.get("pattern", None)
     disable_edited = args.get("disable_edited", False)
