1bb1c1ec8cd0e48848ce6c2c48d1b9697d80344f
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-22 16:11:40 +0530

    Re arranged

--- MashaRoBot/modules/__tools.py
+++ MashaRoBot/modules/__tools.py
@@ -3,8 +3,16 @@
 __mod_name__ = "◎TOOLS"
 
 __help__ = """
- ❍ /encrypt :Encrypts The Given Text
- ❍ /decrypt :Decrypts Previously Ecrypted Text
+*TELEGRAPH*
  ❍ /tm :Get Telegraph Link Of Replied Media
  ❍ /tt :Get Telegraph Link of Replied Text
+
+*Chatbot* 
+_Admins only:_
+ ❍ /addchat*:* Enables Chatbot mode in the chat.
+ ❍ /rmchat*:* Disables Chatbot mode in the chat.
+
+*OTHERS*
+ ❍ /encrypt :Encrypts The Given Text
+ ❍ /decrypt :Decrypts Previously Ecrypted Text
 """
