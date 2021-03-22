c9e1f5f781678ece840440df674d7027d2694e65
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-22 22:12:30 +0530

    Create heroku_helpo.py

---
+++ MashaRoBot/helper_extra/heroku_helpo.py
@@ -0,0 +1,31 @@
+import heroku3
+
+from MashaRoBot import HEROKU_API_KEY, HEROKU_APP_NAME
+
+herokuclient = heroku3.from_key(HEROKU_API_KEY)
+
+
+class HerokuHelper:
+    def __init__(self, appName, apiKey):
+        self.API_KEY = apiKey
+        self.APP_NAME = appName
+        self.herokuclient = self.getherokuclient()
+        self.app = self.herokuclient.apps()[self.APP_NAME]
+
+    def getherokuclient(self):
+        return heroku3.from_key(self.API_KEY)
+
+    def getAccount(self):
+        return self.herokuclient.account()
+
+    def getLog(self):
+        return self.app.get_log()
+
+    def addEnvConfig(self, key, value):
+        self.app.config()[key] = value
+
+    def restart(self):
+        return self.app.restart()
+    
+    def shutdown(self):
+        return self.app.process_formation()["worker.1"].scale(0)
