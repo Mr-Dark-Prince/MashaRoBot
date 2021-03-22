d3685ef36c4ad9c2a263cefb17b7b1c8d53f4f92
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-22 16:23:23 +0530

    🙄🙄long help

--- MashaRoBot/modules/admin.py
+++ MashaRoBot/modules/admin.py
@@ -637,17 +637,20 @@ __help__ = """
  ❍ /promote*:* promotes the user
  ❍ /demote*:* demotes the user
  ❍ /title <title here>*:* sets a custom title for an admin that the bot promoted
- ❍ /setgtitle <newtitle>: Sets new chat title in your group.
- ❍ /setgpic: As a reply to file or photo to set group profile pic!
- ❍ /delgpic: Same as above but to remove group profile pic.
- ❍ /setsticker: As a reply to some sticker to set it as group sticker set!
- ❍ /setdescription: <description> Sets new chat description in group.
+ ❍ /setgtitle <newtitle>*:* Sets new chat title in your group.
+ ❍ /setgpic*:* As a reply to file or photo to set group profile pic!
+ ❍ /delgpic*:* Same as above but to remove group profile pic.
+ ❍ /setsticker*:* As a reply to some sticker to set it as group sticker set!
+ ❍ /setdescription <description>*:* Sets new chat description in group.
  ❍ /admincache*:* force refresh the admins list
  ❍ /antispam <on/off/yes/no>*:* Will toggle our antispam tech or return your current settings.
  ❍ /addnt*:* Adds Group to NightMode Chats
  ❍ /rmnt*:* Removes Group From NightMode Chats
  ❍ /addnsfw*:* Adds The Group to nsfw Watch List
  ❍ /rmnsfw*:* Removes The Group From nsfw Watch List
+ ❍ /del*:* deletes the message you replied to
+ ❍ /purge*:* deletes all messages between this and the replied to message.
+ ❍ /purge <integer X>*:* deletes the replied message, and X messages following it if replied to a message.
 
 *Note:* Night Mode chats get Automatically closed at 12 am(IST)
 and Automatically openned at 6 am(IST) To Prevent Night Spams.
