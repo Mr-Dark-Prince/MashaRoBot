2695053edcaa2a5edc97569835ba00320ee5b2d3
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-22 22:09:28 +0530

    Create __restart.py

---
+++ MashaRoBot/modules/__restart.py
@@ -0,0 +1,24 @@
+import os
+import sys
+from MashaRoBot.function.heroku_helper import HerokuHelper
+import heroku3
+import requests
+from MashaRoBot.events import register
+from MashaRoBot import HEROKU_API_KEY, HEROKU_APP_NAME, OWNER_ID
+Heroku = heroku3.from_key(HEROKU_API_KEY)
+
+@register(pattern="^/restart$")
+async def _(event):
+    if event.fwd_from:
+        return
+    if event.sender_id == OWNER_ID:
+        pass
+    else:
+        return
+    await event.edit("**Masha Restarted🤷**")
+    try:
+        herokuHelper = HerokuHelper(HEROKU_APP_NAME, HEROKU_API_KEY)
+        herokuHelper.restart()
+    except:
+        await borg.disconnect()
+        os.execl(sys.executable, sys.executable, *sys.argv)
