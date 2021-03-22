8881f7f98c7f8061332db9fd2dbe0f6c904174e0
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-22 17:53:19 +0530

    Update __memifytest.py

--- MashaRoBot/modules/__memifytest.py
+++ MashaRoBot/modules/__memifytest.py
@@ -118,20 +118,6 @@ async def add_text_img(image_path, text):
     return final_image
 
 
-async def runcmd(cmd: str) -> tuple[str, str, int, int]:
-    """ run command in terminal """
-    args = shlex.split(cmd)
-    process = await asyncio.create_subprocess_exec(
-        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
-    )
-    stdout, stderr = await process.communicate()
-    return (
-        stdout.decode("utf-8", "replace").strip(),
-        stderr.decode("utf-8", "replace").strip(),
-        process.returncode,
-        process.pid,
-    )
-
 
 async def take_screen_shot(
     video_file: str, duration: int, path: str = ""
