d42a09ce3b1ae3af9db3020d1fdc0fc70db2dff7
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-22 23:59:32 +0530

    Update __tools.py

--- MashaRoBot/modules/__tools.py
+++ MashaRoBot/modules/__tools.py
@@ -18,7 +18,9 @@ _Admins only:_
  ❍ /wttr <city>*:* Advanced weather module, usage same as /weather
  ❍ /wttr moon*:* Get the current status of moon
 
-*Other*
+*Converts*
  ❍ /encrypt*:* Encrypts The Given Text
  ❍ /decrypt*:* Decrypts Previously Ecrypted Text
+ ❍ /zip*:* reply to a telegram file to compress it in .zip format
+ ❍ /unzip*:* reply to a telegram file to decompress it from the .zip format
 """
