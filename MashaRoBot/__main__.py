6477c14087c5b167eb2ee3525f0bce28ccdcc2c2
Mr-Dark-Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-23 12:40:08 +0000

    👣MashaRoBot

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -683,7 +683,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "Yes I'm alive 😹")
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "🔹Restarted🔹 Masha is here 😝")
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!"
