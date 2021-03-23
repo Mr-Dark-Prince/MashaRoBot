2d0dfaf78974defdee6bb5f3f510a482051c91f3
E V I R A L <74889213+Teameviral@users.noreply.github.com>
2021-03-23 23:02:55 +0530

    Update warns.py

--- MashaRoBot/modules/warns.py
+++ MashaRoBot/modules/warns.py
@@ -68,9 +68,7 @@ def warn(
             )
         return
     
-   if user_can_ban(chat, user, context.bot.id) == False: message.reply_text( "You don't have enough rights to warn!" ) return
-
-
+   
     if user.id in WOLVES:
         if warner:
             message.reply_text("Wolf disasters are warn immune.")
@@ -195,6 +193,7 @@ def button(update: Update, context: CallbackContext) -> str:
 @user_admin
 @can_restrict
 @loggable
+@user_can_ban
 def warn_user(update: Update, context: CallbackContext) -> str:
     args = context.args
     message: Optional[Message] = update.effective_message
@@ -221,6 +220,7 @@ def warn_user(update: Update, context: CallbackContext) -> str:
     else:
         message.reply_text("That looks like an invalid User ID to me.")
     return ""
+if user_can_ban(chat, user, context.bot.id) == False: message.reply_text( "You don't have enough rights to warn!" ) return ""
 
 
 @run_async
