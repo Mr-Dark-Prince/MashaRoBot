4a0821c08178fa837a01efb7eca7d5b6caf6e371
Mr-Dark-Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-23 18:05:10 +0000

    👣MashaRoBot

--- MashaRoBot/modules/helper_funcs/chat_status.py
+++ MashaRoBot/modules/helper_funcs/chat_status.py
@@ -367,7 +367,7 @@ def user_can_ban(func):
             and user not in [777000, 1087968824]
         ):
             update.effective_message.reply_text(
-                "Sorry son, but you're not worthy to wield the banhammer."
+                "😹 Sorry You can't do that"
             )
             return ""
         return func(update, context, *args, **kwargs)
