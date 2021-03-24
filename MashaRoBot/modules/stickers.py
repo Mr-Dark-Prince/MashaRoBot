4785b1dccf2048ce68c2f3bbf447e4b65d25c990
E V I R A L <74889213+Teameviral@users.noreply.github.com>
2021-03-24 19:48:05 +0530

    Update stickers.py

--- MashaRoBot/modules/stickers.py
+++ MashaRoBot/modules/stickers.py
@@ -450,13 +450,13 @@ def makepack_internal(
 
 
 __help__ = """
-• `/stickerid`*:* reply to a sticker to me to tell you its file ID.
-• `/getsticker`*:* reply to a sticker to me to upload its raw PNG file.
-• `/kang`*:* reply to a sticker to add it to your pack.
-• `/stickers`*:* Find stickers for given term on combot sticker catalogue
+• /stickerid *:* reply to a sticker to me to tell you its file ID.
+• /getsticker *:* reply to a sticker to me to upload its raw PNG file.
+• /kang *:* reply to a sticker to add it to your pack.
+• /stickers *:* Find stickers for given term on combot sticker catalogue
 """
 
-__mod_name__ = "Stickers"
+__mod_name__ = "STICKERS"
 STICKERID_HANDLER = DisableAbleCommandHandler("stickerid", stickerid)
 GETSTICKER_HANDLER = DisableAbleCommandHandler("getsticker", getsticker)
 KANG_HANDLER = DisableAbleCommandHandler("kang", kang, admin_ok=True)
