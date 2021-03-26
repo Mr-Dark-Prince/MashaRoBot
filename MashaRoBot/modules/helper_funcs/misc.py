f76fdc2e83c2cc11cd2445b09bb5a8cfd6567658
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-26 23:14:29 +0530

    Update misc.py

--- MashaRoBot/modules/helper_funcs/misc.py
+++ MashaRoBot/modules/helper_funcs/misc.py
@@ -62,9 +62,9 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
             ]
         )
 
-    pairs = [modules[i * 3 : (i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)]
+    pairs = [modules[i * 4 : (i + 1) * 4] for i in range((len(modules) + 4 - 1) // 4)]
 
-    round_num = len(modules) / 3
+    round_num = len(modules) / 4
     calc = len(modules) - round(round_num)
     if calc in [1, 2]:
         pairs.append((modules[-1],))
@@ -72,7 +72,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         pairs.append((modules[-1],))
 
     else:
-        pairs += [[EqInlineKeyboardButton("⇪ GO BACK TO THE MAIN MENU ⇪", callback_data="masha_back")]]
+        pairs += [[EqInlineKeyboardButton("⇪ Go back to the main menu ⇪", callback_data="masha_back")]]
 
     return pairs
 
