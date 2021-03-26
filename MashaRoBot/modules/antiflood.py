25cc1703fbd3b031f379ba220180d4af2f0629d7
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-27 02:42:45 +0530

    Update antiflood.py

--- MashaRoBot/modules/antiflood.py
+++ MashaRoBot/modules/antiflood.py
@@ -401,16 +401,10 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = """
-
-*Blue text cleaner* removed any made up commands that people send in your chat.
- ❍ /cleanblue <on/off/yes/no>*:* clean commands after sending
- ❍ /ignoreblue <word>*:* prevent auto cleaning of the command
- ❍ /unignoreblue <word>*:* remove prevent auto cleaning of the command
- ❍ /listblue*:* list currently whitelisted commands
-
 *Antiflood* allows you to take action on users that send more than x messages in a row. Exceeding the set flood \
 will result in restricting that user.
  This will mute users if they send more than 10 messages in a row, bots are ignored.
+
  ❍ /flood*:* Get the current flood control setting
 • *Admins only:*
  ❍ /setflood <int/'no'/'off'>*:* enables or disables flood control
