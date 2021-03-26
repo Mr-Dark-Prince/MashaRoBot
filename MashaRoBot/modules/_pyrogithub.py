d81e793ae5ea9a9c99e39990a2ea644dfa1c3a06
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-27 01:23:49 +0530

    Update _pyrogithub.py

--- MashaRoBot/modules/_pyrogithub.py
+++ MashaRoBot/modules/_pyrogithub.py
@@ -5,8 +5,13 @@ from MashaRoBot import pbot
 from MashaRoBot.pyrogramee.errors import capture_err
 
 
-__mod_name__ = "Github"
+__mod_name__ = "GITHHUB"
 
+__help__ = """
+I will give information about github profile 
+
+ ❍ /github <username>*:* Get information about a GitHub user.
+"""
 
 @pbot.on_message(filters.command('github'))
 @capture_err
