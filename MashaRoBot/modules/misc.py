fcc9be1f213b5391a1bb07ec8e83453d06c8441d
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-27 02:36:24 +0530

    Update misc.py

--- MashaRoBot/modules/misc.py
+++ MashaRoBot/modules/misc.py
@@ -103,29 +103,6 @@ Example:
  `/cash 1 usd inr`
 Output: `1.0 USD = 75.505 INR`
 
-*MATHS*
-Solves complex math problems using https://newton.now.sh
-❍ /math*:* Math `/math 2^2+2(2)`
-❍ /factor*:* Factor `/factor x^2 + 2x`
-❍ /derive*:* Derive `/derive x^2+2x`
-❍ /integrate*:* Integrate `/integrate x^2+2x`
-❍ /zeroes*:* Find 0's `/zeroes x^2+2x`
-❍ /tangent*:* Find Tangent `/tangent 2lx^3`
-❍ /area*:* Area Under Curve `/area 2:4lx^3`
-❍ /cos*:* Cosine `/cos pi`
-❍ /sin*:* Sine `/sin 0`
-❍ /tan*:* Tangent `/tan 0`
-❍ /arccos*:* Inverse Cosine `/arccos 1`
-❍ /arcsin*:* Inverse Sine `/arcsin 0`
-❍ /arctan*:* Inverse Tangent `/arctan 0`
-❍ /abs*:* Absolute Value `/abs -1`
-❍ /log*:* Logarithm `/log 2l8`
-
-_Keep in mind_: To find the tangent line of a function at a certain x value, send the request as c|f(x) where c is the given x value and f(x) is the function expression, the separator is a vertical bar '|'. See the table above for an example request.
-To find the area under a function, send the request as c:d|f(x) where c is the starting x value, d is the ending x value, and f(x) is the function under which you want the curve between the two x values.
-To compute fractions, enter expressions as numerator(over)denominator. For example, to process 2/4 you must send in your expression as 2(over)4. The result expression will be in standard math notation (1/2, 3/4).
-
-💡`Read From Top`
 """
 
 ECHO_HANDLER = DisableAbleCommandHandler("echo", echo, filters=Filters.group)
