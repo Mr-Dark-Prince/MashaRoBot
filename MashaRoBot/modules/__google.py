196bb8d97fa1050497bcd30417a6b0c3f5fa6be8
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-27 01:25:02 +0530

    Update __google.py

--- MashaRoBot/modules/__google.py
+++ MashaRoBot/modules/__google.py
@@ -282,7 +282,6 @@ __help__ = """
  ❍ /img <text>*:* Search Google for images and returns them\nFor greater no. of results specify lim, For eg: `/img hello lim=10`
  ❍ /app <appname>*:* Searches for an app in Play Store and returns its details.
  ❍ /reverse: Does a reverse image search of the media which it was replied to.
- ❍ /country <country name>*:* Gathering info about given country
  ❍ Masha <query>*:* Masha answers the query
   💡Ex: `Masha where is India?`
 """
