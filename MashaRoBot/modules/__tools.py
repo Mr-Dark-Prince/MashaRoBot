741a985c3bfb50d1a01a1b487fa708986d001937
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-27 00:42:42 +0530

    Update __tools.py

--- MashaRoBot/modules/__tools.py
+++ MashaRoBot/modules/__tools.py
@@ -4,9 +4,6 @@ __mod_name__ = "TOOLS"
 
 __help__ = """
 
-*Date-time-Weather*
- ❍ /time <country code>*:* Gives information about a timezone.
-
 *Converts*
  ❍ /encrypt*:* Encrypts The Given Text
  ❍ /decrypt*:* Decrypts Previously Ecrypted Text
