fbf15bca15caf90429b66816f9d8a41a9cf9ef9b
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-27 00:25:01 +0530

    Update __imdb.py

--- MashaRoBot/modules/__imdb.py
+++ MashaRoBot/modules/__imdb.py
@@ -107,3 +107,10 @@ async def imdb(e):
         )
     except IndexError:
         await e.reply("Plox enter **Valid movie name** kthx")
+
+
+__help__ = """
+ ❍ /imdb <Movie name>*:* Get full info about a movie with [imdb.com](https://m.imdb.com)
+"""
+
+__mod_name__ = "IMDB"
