f811739169c424bbefe65dd44391be6e1bccce19
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-27 00:38:14 +0530

    Update __tools.py

--- MashaRoBot/modules/__tools.py
+++ MashaRoBot/modules/__tools.py
@@ -6,9 +6,6 @@ __help__ = """
 
 *Date-time-Weather*
  ❍ /time <country code>*:* Gives information about a timezone.
- ❍ /weather <city>*:* Get weather info in a particular place.
- ❍ /wttr <city>*:* Advanced weather module, usage same as /weather
- ❍ /wttr moon*:* Get the current status of moon
 
 *Converts*
  ❍ /encrypt*:* Encrypts The Given Text
