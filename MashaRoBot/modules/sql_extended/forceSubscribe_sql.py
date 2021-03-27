725541fc81465633e925abb1982506ae9725d187
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-28 01:13:05 +0530

    Update forceSubscribe_sql.py

--- MashaRoBot/modules/sql_extended/forceSubscribe_sql.py
+++ MashaRoBot/modules/sql_extended/forceSubscribe_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Numeric, Boolean
-from sql_helpers import SESSION, BASE
+from sql import SESSION, BASE
 
 class forceSubscribe(BASE):
     __tablename__ = "forceSubscribe"
