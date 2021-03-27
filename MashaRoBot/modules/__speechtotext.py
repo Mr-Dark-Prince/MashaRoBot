90f3606213289dd46040f2ba55dbbfc2a3a472a0
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-27 23:40:27 +0530

    Create __speechtotext.py

---
+++ MashaRoBot/modules/__speechtotext.py
@@ -0,0 +1,27 @@
+from MashaRoBot import telethn as tbot
+import os
+import urllib.request
+from datetime import datetime
+from typing import List
+from typing import Optional
+import requests
+from telethon import *
+from telethon import events
+from telethon.tl import functions
+from telethon.tl import types
+from telethon.tl.types import *
+
+from MashaRoBot import *
+from MashaRoBot.events import register
+
+
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+        return isinstance(
+            (
+                await tbot(functions.channels.GetParticipantRequest(chat, user))
+            ).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
+        )
+    if isinstance(chat, types.InputPeerUser):
+        return True
