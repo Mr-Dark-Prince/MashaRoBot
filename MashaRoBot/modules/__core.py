b3a6447661e05379fb0cd11ef0cf1f9b45235537
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-27 11:15:27 +0530

    Update __core.py

--- MashaRoBot/modules/__core.py
+++ MashaRoBot/modules/__core.py
@@ -79,3 +79,74 @@ async def install(event):
             os.remove(downloaded_file_name)
     await asyncio.sleep(3)
     await event.delete()
+
+from MashaRoBot import telethn as tbot, OWNER_ID, DEV_USERS
+from MashaRoBot.events import register
+import os
+import asyncio
+import os
+import time
+from datetime import datetime
+from MashaRoBot import TEMP_DOWNLOAD_DIRECTORY as path
+from MashaRoBot import TEMP_DOWNLOAD_DIRECTORY
+from datetime import datetime
+import asyncio
+import os
+import time
+from datetime import datetime as dt
+opn = []
+
+@register(pattern="/open")
+async def _(event):
+    xx = await event.reply("Processing...")
+    if event.reply_to_msg_id:
+        a = await event.get_reply_message()
+        if a.media:
+            b = await a.download_media()
+            c = open(b, "r")
+            d = c.read()
+            c.close()
+            n = 4096
+            for bkl in range(0, len(d), n):
+                opn.append(d[bkl : bkl + n])
+            for bc in opn:
+                await event.client.send_message(
+                    event.chat_id,
+                    f"{bc}",
+                    reply_to=event.reply_to_msg_id,
+                )
+            await event.delete()
+            opn.clear()
+            os.remove(b)
+            await xx.delete()
+        else:
+            return await event.reply("Reply to a readable file")
+    else:
+        return await event.reply("Reply to a readable file")
+
+client = tbot
+import time
+from io import BytesIO
+from pathlib import Path
+from MashaRoBot import telethn as borg
+from telethon import functions, types
+from telethon.errors import PhotoInvalidDimensionsError
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.messages import SendMediaRequest
+
+
+@register(pattern="^/makepy ?(.*)")
+async def get(event):
+    name = event.text[5:]
+    if name is None:
+        await event.reply("reply to text message as `.ttf <file name>`")
+        return
+    m = await event.get_reply_message()
+    if m.text:
+        with open(name, "w") as f:
+            f.write(m.message)
+        await event.delete()
+        await event.client.send_file(event.chat_id, name, force_document=True)
+        os.remove(name)
+    else:
+        await event.reply("reply to text message as `.ttf <file name>`")
