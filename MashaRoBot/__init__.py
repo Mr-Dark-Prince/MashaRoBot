0ec56fc4c12f741472f3d36541ae80730c0b32ad
E V I R A L <74889213+Teameviral@users.noreply.github.com>
2021-03-27 23:07:39 +0530

    Update __init__.py

--- MashaRoBot/__init__.py
+++ MashaRoBot/__init__.py
@@ -90,6 +90,8 @@ if ENV:
     SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", None)
     SPAMWATCH_SUPPORT_CHAT = os.environ.get("SPAMWATCH_SUPPORT_CHAT", None)
     SPAMWATCH_API = os.environ.get("SPAMWATCH_API", None)
+    IBM_WATSON_CRED_URL = os.environ.get("IBM_WATSON_CRED_URL", None)
+    IBM_WATSON_CRED_PASSWORD = os.environ.get("IBM_WATSON_CRED_PASSWORD", None)
 
     ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)
 
