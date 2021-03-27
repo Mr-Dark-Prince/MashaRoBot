84e04bc511d054f41052e67b552d658419180131
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-27 14:36:36 +0530

    Create __english.py

---
+++ MashaRoBot/modules/__english.py
@@ -0,0 +1,13 @@
+from MashaRoBot import telethn as tbot
+import json
+import requests
+from MashaRoBot.events import register
+from telethon import *
+from telethon.tl import functions
+import os
+import urllib.request
+from typing import List
+from typing import Optional
+from PyDictionary import PyDictionary
+from telethon.tl import types
+from telethon.tl.types import *
