8373ec355727b67671089d481ce5220e166856f7
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-28 02:12:47 +0530

    Update __forcesub.py

--- MashaRoBot/modules/__forcesub.py
+++ MashaRoBot/modules/__forcesub.py
@@ -1,3 +1,5 @@
+# credits @Damien, @InukaAsith, @Mr_dark_prince
+
 import logging
 import time
 
@@ -178,23 +180,21 @@ def config(client, message):
 
 
 __help__ = """
-*ForceSubscribe:*
+*Force Subscribe:*
 
-Masha can mute members who are not subscribed your channel until they subscribe
-When enabled I will mute unsubscribed members and show them a unmute button. When they pressed the button I will unmute them
+❍ Masha can mute members who are not subscribed your channel until they subscribe
+❍ When enabled I will mute unsubscribed members and show them a unmute button. When they pressed the button I will unmute them
 
 *Setup*
-1) First of all add me in the group as admin with ban users permission and in the channel as admin.
-Note: Only creator of the group can setup me and i will not allow force subscribe again if not done so.
+*Only creator*
+❍ Add me in your group as admin
+❍ Add me in your channel as admin 
  
 *Commmands*
- /ForceSubscribe - To get the current settings.
- /ForceSubscribe no/off/disable - To turn of ForceSubscribe.
- /ForceSubscribe {channel username} - To turn on and setup the channel.
- /ForceSubscribe clear - To unmute all members who muted by me.
-
-Note: /FSub is an alias of /ForceSubscribe
-
- 
+ ❍ /fsub {channel username} - To turn on and setup the channel.
+ ❍ /fsub - To get the current settings.
+ ❍ /fsub disable - To turn of ForceSubscribe..
+ 💡If you disable fsub, you need to set again for working.. /fsub {channel username} 
+ ❍ /fsub clear - To unmute all members who muted by me.
 """
 __mod_name__ = "F SUB"
