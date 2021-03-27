3622278751e16a6955f22315e5e0a7904ce8e48d
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-27 14:39:19 +0530

    Update __english.py

--- MashaRoBot/modules/__english.py
+++ MashaRoBot/modules/__english.py
@@ -11,3 +11,37 @@ from typing import Optional
 from PyDictionary import PyDictionary
 from telethon.tl import types
 from telethon.tl.types import *
+
+
+API_KEY = "6ae0c3a0-afdc-4532-a810-82ded0054236"
+URL = "http://services.gingersoftware.com/Ginger/correct/json/GingerTheText"
+
+
+@register(pattern="^/spell(?: |$)(.*)")
+async def _(event):
+    ctext = await event.get_reply_message()
+    msg = ctext.text
+    #  print (msg)
+    params = dict(lang="US", clientVersion="2.0", apiKey=API_KEY, text=msg)
+
+    res = requests.get(URL, params=params)
+    changes = json.loads(res.text).get("LightGingerTheTextResult")
+    curr_string = ""
+    prev_end = 0
+
+    for change in changes:
+        start = change.get("From")
+        end = change.get("To") + 1
+        suggestions = change.get("Suggestions")
+        if suggestions:
+            sugg_str = suggestions[0].get("Text")
+            curr_string += msg[prev_end:start] + sugg_str
+            prev_end = end
+
+    curr_string += msg[prev_end:]
+    await event.reply(curr_string)
+
+
+dictionary = PyDictionary()
+
+
