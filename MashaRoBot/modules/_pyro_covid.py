5ce5231ac369c1c36cfd885c845e02c6ec59b98a
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-29 16:13:27 +0530

    Create _pyro_covid.py

---
+++ MashaRoBot/modules/_pyro_covid.py
@@ -0,0 +1,5 @@
+from MashaRoBot import pbot as app
+from MashaRoBot.pyrogramee.errors import capture_err
+from MashaRoBot.pyrogramee.json_prettify import json_prettify
+from MashaRoBot.pyrogramee.fetch import fetch
+from pyrogram import filters
