05512a071717ae0ad008b7bdac6eaf4fa8b32157
E V I R A L <74889213+Teameviral@users.noreply.github.com>
2021-03-29 20:55:18 +0530

    Update log_channel.py

--- MashaRoBot/modules/log_channel.py
+++ MashaRoBot/modules/log_channel.py
@@ -197,14 +197,13 @@ if is_module_loaded(FILENAME):
  ❍ /logchannel*:* get log channel info
  ❍ /setlog*:* set the log channel.
  ❍ /unsetlog*:* unset the log channel.
-
 Setting the log channel is done by:
 ❍ adding the bot to the desired channel (as an admin!)
 ❍ sending /setlog in the channel
 ❍ forwarding the /setlog to the group
 """
 
-    __mod_name__ = "CHANNELS"
+    __mod_name__ = "CHANNEL"
 
     LOG_HANDLER = CommandHandler("logchannel", logging)
     SET_LOG_HANDLER = CommandHandler("setlog", setlog)
