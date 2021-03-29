d4d30ce238ebba7588996117808165fd58ff7c4e
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-29 13:21:41 +0530

    Create __json.py

---
+++ MashaRoBot/modules/__json.py
@@ -0,0 +1,7 @@
+import io
+from MashaRoBot.events import register
+from MashaRoBot import tbot as borg
+from telethon import types
+from telethon import events
+from telethon.tl import functions, types
+from telethon.tl.types import *
