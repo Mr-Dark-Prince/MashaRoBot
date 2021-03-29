ea71aa9b46a0a44c589dd932d53789404b9e0a2e
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-29 16:23:23 +0530

    Update and rename _pyro_covid.py to _pyrocovid.py

--- MashaRoBot/modules/_pyro_covid.py
+++
@@ -1,5 +0,0 @@
-from MashaRoBot import pbot as app
-from MashaRoBot.pyrogramee.errors import capture_err
-from MashaRoBot.pyrogramee.json_prettify import json_prettify
-from MashaRoBot.pyrogramee.fetch import fetch
-from pyrogram import filters
---
+++ MashaRoBot/modules/_pyrocovid.py
@@ -0,0 +1,29 @@
+from MashaRoBot import pbot as app
+from MashaRoBot.pyrogramee.errors import capture_err
+from MashaRoBot.pyrogramee.json_prettify import json_prettify
+from MashaRoBot.pyrogramee.fetch import fetch
+from pyrogram import filters
+
+
+@app.on_message(filters.command("covid") & ~filters.edited)
+@capture_err
+async def covid(_, message):
+    if len(message.command) == 1:
+        data = await fetch("https://corona.lmao.ninja/v2/all")
+        data = await json_prettify(data)
+        await app.send_message(message.chat.id, text=data)
+        return
+    if len(message.command) != 1:
+        country = message.text.split(None, 1)[1].strip()
+        country = country.replace(" ", "")
+        data = await fetch(f"https://corona.lmao.ninja/v2/countries/{country}")
+        data = await json_prettify(data)
+        await app.send_message(message.chat.id, text=data)
+        return
+
+
+
+__HELP__ = """
+ ❍ /covid - To Get Global Stats of Covid.
+ ❍ /covid <COUNTRY> - To Get Stats of A Single Country.
+"""
