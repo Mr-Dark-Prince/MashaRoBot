a30dbefff5bccc652407d41620e8b6e964f49e0e
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-30 09:45:46 +0530

    Update night_mode_sql.py

--- MashaRoBot/modules/sql_extended/night_mode_sql.py
+++ MashaRoBot/modules/sql_extended/night_mode_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
-from Evie.modules.sql import BASE, SESSION
+from MashaRoBot.modules.sql import BASE, SESSION
 
 
 class Nightmode(BASE):
