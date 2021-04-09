668e226c5d041e396862e2ff20e59524f20c172e
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-04-09 11:44:55 +0530

    Update _pyrocovid.py

--- MashaRoBot/modules/_pyrocovid.py
+++ MashaRoBot/modules/_pyrocovid.py
@@ -22,9 +22,3 @@ async def covid(_, message):
         return
 
 
-__HELP__ = """
- ❍ /covid - To Get Global Stats of Covid.
- ❍ /covid <country> - To Get Stats of A Single Country.
-"""
-
-__mod_name__ = "COVID"
