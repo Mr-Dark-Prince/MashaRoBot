19ad1210e602d0b14554754d377dc9160c722775
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-04-16 17:27:30 +0530

    Update anime.py

--- MashaRoBot/modules/anime.py
+++ MashaRoBot/modules/anime.py
@@ -565,7 +565,7 @@ dispatcher.add_handler(KAIZOKU_SEARCH_HANDLER)
 dispatcher.add_handler(KAYO_SEARCH_HANDLER)
 dispatcher.add_handler(UPCOMING_HANDLER)
 
-__mod_name__ = "ANIME"
+__mod_name__ = "Anime"
 __command_list__ = [
     "anime",
     "manga",
