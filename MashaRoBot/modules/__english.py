926978b14644f958a469e6140e40a5fb723c1024
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-04-16 21:12:31 +0530

    Update __english.py

--- MashaRoBot/modules/__english.py
+++ MashaRoBot/modules/__english.py
@@ -83,10 +83,10 @@ async def _(event):
 
 
 __help__ = """
- ❍ /define <text>*:* Type the word or expression you want to search\nFor example /define kill
- ❍ /spell*:* while replying to a message, will reply with a grammar corrected version
- ❍ /synonyms <word>*:* Find the synonyms of a word
- ❍ /antonyms <word>*:* Find the antonyms of a word
+• `/define` `<text>`*:* Type the word or expression you want to search\nFor example /define kill
+• `/spell`*:* while replying to a message, will reply with a grammar corrected version
+• `/synonyms` `<word>`*:* Find the synonyms of a word
+• `/antonyms` `<word>`*:* Find the antonyms of a word
 """
 
 __mod_name__= "ENGLISH"
