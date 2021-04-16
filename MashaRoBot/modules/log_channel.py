e1540b5e66317520176bf835f1abf0d9aac00b58
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-04-16 18:44:34 +0530

    Update log_channel.py

--- MashaRoBot/modules/log_channel.py
+++ MashaRoBot/modules/log_channel.py
@@ -193,15 +193,15 @@ if is_module_loaded(FILENAME):
         return "No log channel is set for this group!"
 
     __help__ = """
-*Admins only:*
- ❍ /logchannel*:* get log channel info
- ❍ /setlog*:* set the log channel.
- ❍ /unsetlog*:* unset the log channel.
+*Admins only* *:*
+• `/logchannel`*:* get log channel info
+• `/setlog`*:* set the log channel.
+• `/unsetlog`*:* unset the log channel.
 
 Setting the log channel is done by:
-❍ adding the bot to the desired channel (as an admin!)
-❍ sending /setlog in the channel
-❍ forwarding the /setlog to the group
+• adding the bot to the desired channel (as an admin ofc!)
+• sending /setlog in the channel
+• forwarding the /setlog to the group
 """
 
     __mod_name__ = "CHANNEL"
