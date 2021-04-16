303dfcd458a99d21077cf8b24fd0bc786bb13a33
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-04-16 18:37:46 +0530

    Update gettime.py

--- MashaRoBot/modules/gettime.py
+++ MashaRoBot/modules/gettime.py
@@ -92,11 +92,12 @@ def gettime(update: Update, context: CallbackContext):
 
 
 __help__ = """
- ❍ /time <query>*:* Gives information about a timezone.
+• `/time` `<query>`*:* Gives information about a timezone.
 *Available queries:* Country Code/Country Name/Timezone Name
- ❍ ⏰ [Timezones list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)
 
-💡 Ex:- /time IN *:* It will shows Indian current time and date..
+• ⏰ Timezones [list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)
+
+💡 Ex:- `/time IN` *:* It will shows Indian current time and date.
 """
 
 TIME_HANDLER = DisableAbleCommandHandler("time", gettime)
