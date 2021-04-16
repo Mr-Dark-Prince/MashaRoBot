a09cb7e09c3e4e32c85ccb4bb45b470da59b258e
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-04-16 13:08:25 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -346,7 +346,7 @@ def Masha_about_callback(update: Update, context: CallbackContext):
     query = update.callback_query
     if query.data == "masha_":
         query.message.edit_text(
-            text=""" ℹ️ I'm *Senku*, a powerful group management bot built to help you manage your group easily.
+            text="""⚡️ I'm *Senku*, a powerful group management bot built to help you manage your group easily.
 
 • I can restrict users.
 • I can greet users with customizable welcome messages and even set a group's rules.
