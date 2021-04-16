08aed9abc4e18f1dfbc566dca736fb94eb28b118
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-04-16 21:54:51 +0530

    Update __google.py

--- MashaRoBot/modules/__google.py
+++ MashaRoBot/modules/__google.py
@@ -278,10 +278,10 @@ async def apk(e):
 __mod_name__ = "GOOGLE"
 
 __help__ = """
-• `/google` `<text>`*:* Perform a google search
-• `/img `<text>`*:* Search Google for images and returns them\nFor greater no. of results specify lim, For eg: `/img hello lim=10`
-• `/app` `<appname>`*:* Searches for an app in Play Store and returns its details.
-• `/reverse`*:* Does a reverse image search of the media which it was replied to.
- • `Masha` `<query>`*:* Senku answers the query
+ ❍ /google <text>*:* Perform a google search
+ ❍ /img <text>*:* Search Google for images and returns them\nFor greater no. of results specify lim, For eg: `/img hello lim=10`
+ ❍ /app <appname>*:* Searches for an app in Play Store and returns its details.
+ ❍ /reverse: Does a reverse image search of the media which it was replied to.
+ ❍ Masha <query>*:* Masha answers the query
   💡Ex: `Masha where is India?`
 """
