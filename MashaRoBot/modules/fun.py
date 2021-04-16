45024e9f7b4fe31641bf5aefa333b08b717daab8
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-04-16 20:42:25 +0530

    Update fun.py

--- MashaRoBot/modules/fun.py
+++ MashaRoBot/modules/fun.py
@@ -325,20 +325,20 @@ def weebify(update: Update, context: CallbackContext):
 
 
 __help__ = """
- ❍ /runs*:* reply a random string from an array of replies
- ❍ /slap*:* slap a user, or get slapped if not a reply
- ❍ /shrug*:* get shrug XD
- ❍ /table*:* get flip/unflip :v
- ❍ /decide*:* Randomly answers yes/no/maybe
- ❍ /toss*:* Tosses A coin
- ❍ /bluetext*:* check urself :V
- ❍ /roll*:* Roll a dice
- ❍ /rlg*:* Join ears,nose,mouth and create an emo ;-;
- ❍ /shout <keyword>*:* write anything you want to give loud shout
- ❍ /weebify <text>*:* returns a weebified text
- ❍ /sanitize*:* always use this before /pat or any contact
- ❍ /pat*:* pats a user, or get patted
- ❍ /8ball*:* predicts using 8ball method 
+• `/runs`*:* reply a random string from an array of replies
+• `/slap`*:* slap a user, or get slapped if not a reply
+• `/shrug`*:* get shrug XD
+• `/table`*:* get flip/unflip :v
+• `/decide`*:* Randomly answers yes/no/maybe
+• `/toss`*:* Tosses A coin
+• `/bluetext`*:* check urself :V
+• `/roll`*:* Roll a dice
+• `/rlg`*:* Join ears,nose,mouth and create an emo ;-;
+• `/shout` `<keyword>`*:* write anything you want to give loud shout
+• `/weebify` `<text>`*:* returns a weebified text
+• `/sanitize`*:* always use this before /pat or any contact
+• `/pat`*:* pats a user, or get patted
+• `/8ball`*:* predicts using 8ball method 
 """
 
 SANITIZE_HANDLER = DisableAbleCommandHandler("sanitize", sanitize)
