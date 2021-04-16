9063d1643cfe155216c8945e161d2f17939e7503
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-04-16 21:42:41 +0530

    Update __aifunctions.py

--- MashaRoBot/modules/__aifunctions.py
+++ MashaRoBot/modules/__aifunctions.py
@@ -26,7 +26,7 @@ async def hmm(event):
   lodu = response.json()
   result = (lodu['message']['text'])
   if "no no" in result:
-   pro = "It's exhilarating | Get Excited!! ~ @SenkuSupport."
+   pro = "It's exhilarating | Get Excited!"
    try:
       async with telethn.action(event.chat_id, 'typing'):
            await asyncio.sleep(2)
