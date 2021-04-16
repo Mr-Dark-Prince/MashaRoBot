361e6619c34aeaaad9cf7aa7b51fb7c7ff02b1d1
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-04-16 18:03:51 +0530

    Update antiflood.py

--- MashaRoBot/modules/antiflood.py
+++ MashaRoBot/modules/antiflood.py
@@ -401,18 +401,18 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = """
-*Antiflood* allows you to take action on users that send more than x messages in a row. Exceeding the set flood \n
+*Antiflood* allows you to take action on users that send more than x messages in a row. Exceeding the set flood \
 will result in restricting that user.
  This will mute users if they send more than 10 messages in a row, bots are ignored.
 
-• `/flood`*:* Get the current flood control setting
+ ❍ `/flood`*:* Get the current flood control setting
 • *Admins only:*
- - `/setflood `<int/'no'/'off'>`*:* enables or disables flood control
+ ❍ `/setflood` `<int/'no'/'off'>`*:* enables or disables flood control
  *Example:* `/setflood 10`
- - `/setfloodmode` `<ban/kick/mute/tban/tmute>` `<value>`*:* Action to perform when user have exceeded flood limit. ban/kick/mute/tmute/tban
+ ❍ `/setfloodmode` `<ban/kick/mute/tban/tmute>` `<value>`*:* Action to perform when user have exceeded flood limit. ban/kick/mute/tmute/tban
 • *Note:*
- - Value must be filled for tban and tmute!!
- It can be :
+ • Value must be filled for tban and tmute!!
+ It can be:
  `5m` = 5 minutes
  `6h` = 6 hours
  `3d` = 3 days
