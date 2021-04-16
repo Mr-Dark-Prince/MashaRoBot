f4c855a9831dc348aba0fa50ae686a93b6eacad9
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-04-16 17:36:43 +0530

    Update blacklist.py

--- MashaRoBot/modules/blacklist.py
+++ MashaRoBot/modules/blacklist.py
@@ -449,7 +449,7 @@ def __stats__():
     )
 
 
-__mod_name__ = "B LIST"
+__mod_name__ = "Blacklists"
 
 __help__ = """
 
@@ -457,12 +457,12 @@ Blacklists are used to stop certain triggers from being said in a group. Any tim
 
 *NOTE*: Blacklists do not affect group admins.
 
- ❍ /blacklist*:* View the current blacklisted words.
+• `/blacklist`*:* View the current blacklisted words.
 
 Admin only:
- ❍ /addblacklist <triggers>*:* Add a trigger to the blacklist. Each line is considered one trigger, so using different lines will allow you to add multiple triggers.
- ❍ /unblacklist <triggers>*:* Remove triggers from the blacklist. Same newline logic applies here, so you can remove multiple triggers at once.
- ❍ /blacklistmode <off/del/warn/ban/kick/mute/tban/tmute>*:* Action to perform when someone sends blacklisted words.
+• `/addblacklist` `<triggers>`*:* Add a trigger to the blacklist. Each line is considered one trigger, so using different lines will allow you to add multiple triggers.
+• `/unblacklist` `<triggers>`*:* Remove triggers from the blacklist. Same newline logic applies here, so you can remove multiple triggers at once.
+• `/blacklistmode` `<off/del/warn/ban/kick/mute/tban/tmute>`*:* Action to perform when someone sends blacklisted words.
 """
 
 BLACKLIST_HANDLER = DisableAbleCommandHandler(
