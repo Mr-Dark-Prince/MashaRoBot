7b73847a8c48da6aebf6fa9a9b6feefab6c81b60
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-04-16 18:00:29 +0530

    Update chatbot.py

--- MashaRoBot/modules/chatbot.py
+++ MashaRoBot/modules/chatbot.py
@@ -169,7 +169,7 @@ dispatcher.add_handler(REMOVE_CHAT_HANDLER)
 dispatcher.add_handler(CHATBOT_HANDLER)
 dispatcher.add_handler(LIST_CB_CHATS_HANDLER)
 
-__mod_name__ = "Chatbot"
+__mod_name__ = "CHATBOT"
 __command_list__ = ["addchat", "rmchat", "listaichats"]
 __handlers__ = [
     ADD_CHAT_HANDLER,
