db8015b0c23aabeebc8fc787158f8542e09a58ff
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-04-16 21:35:21 +0530

    Update welcome.py

--- MashaRoBot/modules/welcome.py
+++ MashaRoBot/modules/welcome.py
@@ -1097,7 +1097,7 @@ __help__ = """
 • `/welcome` `<on/off>`*:* enable/disable welcome messages.
 • `/welcome`*:* shows current welcome settings.
 • `/welcome noformat`*:* shows current welcome settings, without the formatting - useful to recycle your welcome messages!
-• `/goodbye*:* same usage and args as `/welcome`.
+• `/goodbye`*:* same usage and args as `/welcome`.
 • `/setwelcome` `<sometext>`*:* set a custom welcome message. If used replying to media, uses that media.
 • `/setgoodbye` `<sometext>`*:* set a custom goodbye message. If used replying to media, uses that media.
 • `/resetwelcome`*:* reset to the default welcome message.
@@ -1144,7 +1144,7 @@ dispatcher.add_handler(CLEAN_SERVICE_HANDLER)
 dispatcher.add_handler(BUTTON_VERIFY_HANDLER)
 dispatcher.add_handler(WELCOME_MUTE_HELP)
 
-__mod_name__ = "GREETINGS"
+__mod_name__ = "ARRIVAL"
 __command_list__ = []
 __handlers__ = [
     NEW_MEM_HANDLER,
