436c9f810444803b9e63dce4eafbb3427024225e
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-04-16 21:07:18 +0530

    Update __musicals.py

--- MashaRoBot/modules/__musicals.py
+++ MashaRoBot/modules/__musicals.py
@@ -1,7 +1,7 @@
 __mod_name__ = "MUSIC"
 
 __help__ = """
- ❍ /song <song name>*:* Uploads the song in it's best quality available
+• `/song` `<song name>`*:* Uploads the song in it's best quality available
  💡Ex: `/song Faded Alan Walker`
- ❍ /saavn <song name>*:* Download song from saavn.
+• `/saavn` `<song name>`*:* Download song from saavn.
 """
