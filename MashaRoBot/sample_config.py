cf75d0339fa8e12a90904f82813e61fb2744b461
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-04-16 09:08:44 +0530

    Update sample_config.py

--- MashaRoBot/sample_config.py
+++ MashaRoBot/sample_config.py
@@ -17,9 +17,9 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Sawada"
-    SUPPORT_CHAT = "OnePunchSupport"  # Your own group for support, do not add the @
+    OWNER_ID = 1643262660  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "FtConan"
+    SUPPORT_CHAT = "SenkuSupport"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001253661229
     )  # Prints any new group the bot is added to, prints just the name and ID.
