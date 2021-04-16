3e702e1fae83208a72bd702305c9532b6d05695c
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-04-16 21:04:32 +0530

    Update __polling.py

--- MashaRoBot/modules/__polling.py
+++ MashaRoBot/modules/__polling.py
@@ -397,13 +397,13 @@ __help__ = """
 You can now send polls anonymously with Masha
 Here is how you can do it:
 **Parameters** -
- ❍ poll-id - a poll id consists of an 5 digit random integer, this id is automatically removed from the system when you stop your previous poll
- ❍ question - the question you wanna ask
- ❍ <True@optionnumber/False>(1) - quiz mode, you must state the correct answer with `@` eg: `True@1` or `True@2`
- ❍ <True/False>(2) - public votes
- ❍ <True/False>(3) - multiple choice
+• poll-id - a poll id consists of an 5 digit random integer, this id is automatically removed from the system when you stop your previous poll
+• question - the question you wanna ask
+• <True@optionnumber/False>(1) - quiz mode, you must state the correct answer with `@` eg: `True@1` or `True@2`
+• <True/False>(2) - public votes
+• <True/False>(3) - multiple choice
 **Syntax** -
-❍ `/poll <poll-id> <question> | <True@optionnumber/False> <True/False> <True/False> <option1> <option2> ... upto <option10>`
+• `/poll <poll-id> <question> | <True@optionnumber/False> <True/False> <True/False> <option1> <option2> ... upto <option10>`
 **Examples** -
 ❍ `/poll 12345 | am i cool? | False False False yes no`
 ❍ `/poll 12345 | am i cool? | True@1 False False yes no`
