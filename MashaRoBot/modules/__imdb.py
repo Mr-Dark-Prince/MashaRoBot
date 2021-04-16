ee73bce4215f98d23a5900740b837d51215a54ac
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-04-16 21:08:04 +0530

    Update __imdb.py

--- MashaRoBot/modules/__imdb.py
+++ MashaRoBot/modules/__imdb.py
@@ -110,7 +110,7 @@ async def imdb(e):
 
 
 __help__ = """
- ❍ /imdb <Movie name>*:* Get full info about a movie with [imdb.com](https://m.imdb.com)
+• `/imdb` `<Movie name>`*:* Get full info about a movie with [imdb.com](https://m.imdb.com)
 """
 
 __mod_name__ = "IMDB"
