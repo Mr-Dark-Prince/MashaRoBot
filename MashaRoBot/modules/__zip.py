4d011d7920f9165e741db59dbf6c10066362469b
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-04-16 20:58:11 +0530

    Update __zip.py

--- MashaRoBot/modules/__zip.py
+++ MashaRoBot/modules/__zip.py
@@ -211,8 +211,8 @@ def get_lst_of_files(input_directory, output_lst):
 
 __help__ = """
 Hey I can convert files here..
- ❍ /zip*:* reply to a telegram file to compress it in .zip format
- ❍ /unzip*:* reply to a telegram file to decompress it from the .zip format
+• `/zip`*:* reply to a telegram file to compress it in .zip format
+• `/unzip`*:* reply to a telegram file to decompress it from the .zip format
 """
 
 __mod_name__ = "ZIPPER"
