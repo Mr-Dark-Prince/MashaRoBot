ad1a958a6804746399011b1cad5f69beaf5c9e53
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-04-17 09:33:10 +0530

    Update __google.py

--- MashaRoBot/modules/__google.py
+++ MashaRoBot/modules/__google.py
@@ -304,6 +304,4 @@ __help__ = """
 • `/img` `<text>`*:* Search Google for images and returns them\nFor greater no. of results specify lim, For eg: `/img hello lim=10`
 • `/app` `<appname>`*:* Searches for an app in Play Store and returns its details.
 • `/reverse`*:* Does a reverse image search of the media which it was replied to.
-• `Senku` `<query>`*:* Senku answers the query
-  💡Ex: `Senku where is India?`
 """
