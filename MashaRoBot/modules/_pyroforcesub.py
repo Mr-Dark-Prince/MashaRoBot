2d219a51fb2bf5c33828d40a611596b481f849c6
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-04-17 09:13:01 +0530

    Update _pyroforcesub.py

--- MashaRoBot/modules/_pyroforcesub.py
+++ MashaRoBot/modules/_pyroforcesub.py
@@ -185,17 +185,16 @@ __help__ = """
 • Senku can mute members who are not subscribed your channel until they subscribe
 • When enabled I will mute unsubscribed members and show them a unmute button. When they pressed the button I will unmute them
 
-*Setup*
-*Only creator*
+*Setup* | *Only creator*
 • Add me in your group as admin
 • Add me in your channel as admin 
  
 *Commmands*
 • `/fsub` `{channel username}`*:* To turn on and setup the channel.
-💡Do this first...
- ❍ `/fsub`*:* To get the current settings.
- ❍ `/fsub disable`*:* To turn of ForceSubscribe..
- 💡If you disable fsub, you need to set again for working. `/fsub` `{channel username}` 
- ❍ `/fsub clear`*:* To unmute all members who muted by me.
+ 💡Do this first...
+ • `/fsub`*:* To get the current settings.
+ • `/fsub disable`*:* To turn of ForceSubscribe..
+ 💡If you disable fsub, you need to set again for working.. /fsub {channel username} 
+ • `/fsub clear`*:* To unmute all members who muted by me.
 """
 __mod_name__ = "F SUB"
