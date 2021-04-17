cbf4ebcb32cd515f4f4daa3b9a0ca0d2410beb8c
Haise Sasaki <62333331+FtSasaki@users.noreply.github.com>
2021-04-17 07:16:10 +0530

    Update __aifunctions.py

--- MashaRoBot/modules/__aifunctions.py
+++ MashaRoBot/modules/__aifunctions.py
@@ -7,7 +7,7 @@ from telethon import types
 from telethon.tl import functions
 import asyncio, os
 
-@register(pattern="Masha (.*)")
+@register(pattern="Senku (.*)")
 async def hmm(event):
   test = event.pattern_match.group(1)
   r = ('\n    \"consent\": true,\n    \"ip\": \"::1\",\n    \"question\": \"{}\"\n').format(test)
@@ -26,7 +26,7 @@ async def hmm(event):
   lodu = response.json()
   result = (lodu['message']['text'])
   if "no no" in result:
-   pro = "I am fairly yound and I was made by @Mr_Dark_Prince."
+   pro = "Get Excited!"
    try:
       async with telethn.action(event.chat_id, 'typing'):
            await asyncio.sleep(2)
@@ -34,7 +34,7 @@ async def hmm(event):
    except CFError as e:
            print(e)
   elif "ann" in result:
-   pro = "My name is Masha"
+   pro = "I am Senku!"
    try:
       async with telethn.action(event.chat_id, 'typing'):
            await asyncio.sleep(2)
