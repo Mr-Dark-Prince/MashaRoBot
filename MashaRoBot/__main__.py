f0aeb94b89bef9624b973c81ee149e1f39c95a55
Vedant-VN <81044922+Vedant-VN@users.noreply.github.com>
2021-04-19 21:31:25 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -74,7 +74,7 @@ def get_readable_time(seconds: int) -> str:
 
 
 PM_START_TEXT = """
-Hello! I am *Senku*.
+Hello! I am *Naruto*.
 I'm here to help you manage your groups! Hit *📚Commands* button below to find out more about how to use me to my full potential[.](https://telegra.ph/file/418fe04d27f1fb02788e3.png)
 """
 
