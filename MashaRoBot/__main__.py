98f51bc1c094d97bd4d7cb50523ac9c00bca6bbd
Vedant-VN <81044922+Vedant-VN@users.noreply.github.com>
2021-04-20 20:05:24 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -220,7 +220,7 @@ def start(update: Update, context: CallbackContext):
             )
     else:
         update.effective_message.reply_text(
-            "I'm awake already practicing the new jutsu I learnt!\n<b>Haven't rested since:</b> <code>{}</code>".format(
+            "I'm awake already Just practicing the new jutsu I learnt!\n<b>Haven't rested since:</b> <code>{}</code>".format(
                 uptime
             ),
             parse_mode=ParseMode.HTML,
