a7743a204cf69226d3a246a635776d2bc02f7da9
Vedant-VN <81044922+Vedant-VN@users.noreply.github.com>
2021-04-24 19:54:01 +0530

    Update afk.py
    changed AFKs to Naruto Themes

--- MashaRoBot/modules/afk.py
+++ MashaRoBot/modules/afk.py
@@ -38,7 +38,7 @@ def afk(update: Update, context: CallbackContext):
     sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
     try:
-        update.effective_message.reply_text("{} is now away!{}".format(fname, notice))
+        update.effective_message.reply_text("User {} is now away! Let's Not Disturb them {}".format(fname, notice))
     except BadRequest:
         pass
 
@@ -58,13 +58,13 @@ def no_longer_afk(update: Update, context: CallbackContext):
         firstname = update.effective_user.first_name
         try:
             options = [
-                "{} is here!",
-                "{} is back!",
-                "{} is now in the chat!",
-                "{} is awake!",
-                "{} is back online!",
-                "{} is finally here!",
-                "Welcome back! {}",
+                "Summoning Jutsu...{} is here Now!",
+                "Fear Not Citizens {} is back Online!",
+                "Hey Sakura it's Not Our Sasuke...it's Just {} Back Online!",
+                "OwO Finally {} is awake!",
+                "UwU Welcome Back {} Now Give Me Some Ramen!",
+                "{} is finally Out Of Itachi's Genjutsu and back here!",
+                "Calm Down KURAMA...it's Just {} Back In the Chat!",
                 "Where is {}?\nIn the chat!",
             ]
             chosen_option = random.choice(options)
