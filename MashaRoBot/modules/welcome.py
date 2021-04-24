09834c062f2ac27e211b6ac98013348336d9fc8e
Vedant-VN <81044922+Vedant-VN@users.noreply.github.com>
2021-04-24 20:03:01 +0530

    Update welcome.py
    basic

--- MashaRoBot/modules/welcome.py
+++ MashaRoBot/modules/welcome.py
@@ -497,7 +497,7 @@ def check_not_bot(member, chat_id, message_id, context):
 
         try:
             bot.edit_message_text(
-                "*kicks user*\nThey can always rejoin and try.",
+                "*Rasengans user*\nNoob Ninja failed Captcha They can always rejoin and try.",
                 chat_id=chat_id,
                 message_id=message_id,
             )
