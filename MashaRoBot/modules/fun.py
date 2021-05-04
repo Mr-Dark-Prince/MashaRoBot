193772a1bff11497a24ea9658985b5a4bac21a3b
Vedant-VN <81044922+Vedant-VN@users.noreply.github.com>
2021-05-04 17:49:46 +0530

    Update fun.py
    fun

--- MashaRoBot/modules/fun.py
+++ MashaRoBot/modules/fun.py
@@ -371,7 +371,7 @@ dispatcher.add_handler(DECIDE_HANDLER)
 dispatcher.add_handler(EIGHTBALL_HANDLER)
 dispatcher.add_handler(TABLE_HANDLER)
 
-__mod_name__ = "MEMES"
+__mod_name__ = "NARUTO FUN"
 __command_list__ = [
     "runs",
     "slap",
