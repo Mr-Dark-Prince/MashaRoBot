94b014b87a50dc1814d78e90dc68200a7c037122
Vedant-VN <81044922+Vedant-VN@users.noreply.github.com>
2021-05-04 17:51:23 +0530

    Update _pyrogithub.py
    github

--- MashaRoBot/modules/_pyrogithub.py
+++ MashaRoBot/modules/_pyrogithub.py
@@ -5,7 +5,7 @@ from MashaRoBot import pbot
 from MashaRoBot.pyrogramee.errors import capture_err
 
 
-__mod_name__ = "GITHHUB"
+__mod_name__ = "GITHUB"
 
 __help__ = """
 I will give information about github profile 
