6c2fd58914f67aef9756fec01cc5171a2f8b20fa
Vedant-VN <81044922+Vedant-VN@users.noreply.github.com>
2021-05-12 19:12:07 +0530

    updated

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -700,7 +700,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "Sleep Time Over! I'M BACK OwO")
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "Sleep Time Over! I'M Back OwO")
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!"
