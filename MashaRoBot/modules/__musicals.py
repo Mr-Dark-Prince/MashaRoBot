30ca31427b6304946cd86bcd4105ca02ac9f0a24
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-06-13 14:52:01 +0530

    Delete MashaRoBot directory

--- MashaRoBot/modules/__musicals.py
+++
@@ -1,7 +0,0 @@
-__mod_name__ = "MUSIC"
-
-__help__ = """
- ❍ /song <song name>*:* Uploads the song in it's best quality available
- 💡Ex: `/song Faded Alan Walker`
- ❍ /saavn <song name>*:* Download song from saavn.
-"""
