2fd951287572e416704540f50ba4e1b095790686
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-12-04 19:23:23 +0530

    Update permissions.py

--- EvaMariaRobot/core/decorators/permissions.py
+++ EvaMariaRobot/core/decorators/permissions.py
@@ -1,34 +1,11 @@
-"""
-MIT License
-
-Copyright (c) 2021 TheHamkerCat
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
 from functools import wraps
 from traceback import format_exc as err
 
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 from pyrogram.types import Message
 
-from wbb import SUDOERS, app
-from wbb.modules.admin import member_permissions
+from EvaMariaRobot import SUDOERS, app
+from EvaMariaRobot.modules.admin import member_permissions
 
 
 async def authorised(func, subFunc2, client, message, *args, **kwargs):
