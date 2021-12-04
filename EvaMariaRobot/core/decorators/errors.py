0522458c1a62c253bbabc3bc0236643402c4e3b6
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-12-04 19:21:57 +0530

    Update errors.py

--- EvaMariaRobot/core/decorators/errors.py
+++ EvaMariaRobot/core/decorators/errors.py
@@ -6,7 +6,7 @@ from functools import wraps
 
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
-from wbb import LOG_GROUP_ID, app
+from EvaMariaRobot import LOG_GROUP_ID, app
 
 
 def split_limits(text):
