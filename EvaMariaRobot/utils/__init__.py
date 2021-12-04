ef8c24286698696a827d89b69755e17c5e66ddd7
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-12-04 21:39:22 +0530

    Delete EvaMariaRobot directory

--- EvaMariaRobot/utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from .misc import paginate_modules  # noqa: F401
-from .read_lines import random_line  # noqa: F401
--- EvaMariaRobot/utils/read_lines.py
+++
@@ -1,7 +0,0 @@
-from random import choice
-
-
-async def random_line(fname):
-    with open(fname) as f:
-        data = f.read().splitlines()
-    return choice(data)
